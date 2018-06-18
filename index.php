6cb71c985505a32bbae77fd679d1c935ffa8e7a1
Firore <takbasic722@gmail.com>
2018-06-18 23:48:40 +0700

    งง ชิบหายเลย

--- index.php
+++ index.php
@@ -25,12 +25,12 @@ session_start();
       ?>    </head>
     <body>
         <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-      <a class="navbar-brand" href="#">องค์การสวนสัตว์</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarCollapse">
-        <ul class="navbar-nav mr-auto">
+			<a class="navbar-brand" href="#">องค์การสวนสัตว์</a>
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+				</button>
+			<div class="collapse navbar-collapse" id="navbarCollapse">
+				<ul class="navbar-nav mr-auto">
 <!--           <li class="nav-item active">
             <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
           </li>
@@ -40,80 +40,67 @@ session_start();
           <li class="nav-item">
             <a class="nav-link disabled" href="#">Disabled</a>
           </li> -->
-        </ul>
-        <div class="form-inline mt-2 mt-md-0">
-          
-          <a class="btn btn-outline-success my-2 my-sm-0" href="login.php">เข้าสู่ระบบ</a>
-        </div>
-      </div>
-    </nav>
+				</ul>
+				<div class="form-inline mt-2 mt-md-0">
+					<a class="btn btn-outline-success my-2 my-sm-0" href="login.php">เข้าสู่ระบบ</a>
+				</div>
+			</div>
+		</nav>
+		<main role="main" class="container">
+			<div class="row">
+				<?php include('menu_main.php'); ?>
+				<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" style="padding-left:150px;">
+					<div class="jumbotron">
+						<h3>ข่าวสารภายในองค์การสวนสัตว์</h3>
+						<div class="container marketing">
 
-    <main role="main" class="container">
-        <div class="row">
-        <div class="col-xl-3 col-lg-4 col-md-5 col-sm-12 col-12">
-            <?php include('menu_main.php'); ?> 
-        </div>
+						<!-- Three columns of text below the carousel -->
+						<div class="row">
+						  <div class="col-lg-4">
+							<img class="img-thumbnail" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
+							<h4>Heading</h4>
+							<p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
+							<p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+						  </div><!-- /.col-lg-4 -->
+						  <div class="col-lg-4">
+							<img class="img-thumbnail" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
+							<h4>Heading</h4>
+							<p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
+							<p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+						  </div><!-- /.col-lg-4 -->
+						  <div class="col-lg-4">
+							<img class="img-thumbnail" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
+							<h4>Heading</h4>
+							<p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+							<p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+						  </div><!-- /.col-lg-4 -->
+						</div><!-- /.row -->
+						</div>
+					</div>
+				</div>
+				<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" style="padding-left:350px;">
+					<center>
+						<div class="indexft"><a href="http://www.zoothailand.org" target="_blank"><img src="images/Logo/ZPO.png"/></a></div>
+						<div class="indexft"><a href="http://www.dusitzoo.org" target="_blank"><img src="images/Logo/Dusitzoo.png"></a></div>
+						<div class="indexft"><a href="http://kkopenzoo.com/newversion_index.php"><img src="images/Logo/KKOzoo.png"></a></div>
+						<div class="indexft"><a href="http://www.chiangmaizoo.com" target="_blank"><img src="images/Logo/chiangmai.png"></a></div>
+						<div class="indexft"><a href="http://www.koratzoo.org" target="_blank"><img src="images/Logo/Nakhonrachsimazoo.png"></a></div>
+						<div class="indexft"><a href="http://www.songkhlazoo.com" target="_blank"><img src="images/Logo/Songkhlazoo.png"></a></div>
+						<div class="indexft"><a href="http://www.ubon-zoo.com" target="_blank"><img src="images/Logo/Ubonzoo.png"></a></div>
+						<div class="indexft"><a href="http://www.khonkaenzoo.com" target="_blank"><img src="images/Logo/KKzoo.png"></a></div>
+						<div class="indexft"><a href="#" target="_blank"><img src="images/Logo/surin.png"></a></div>
+					</center>
+				</div>
+			</div>
+		</main>
         
-    <div class="col-xl-9 col-lg-8 col-md-7 col-sm-12 col-12">
-      <div class="jumbotron">
-        <h3>ข่าวสารภายในองค์การสวนสัตว์</h3>
-        <div class="container marketing">
-
-        <!-- Three columns of text below the carousel -->
-        <div class="row">
-          <div class="col-lg-4">
-            <img class="img-thumbnail" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
-            <h4>Heading</h4>
-            <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
-            <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
-          </div><!-- /.col-lg-4 -->
-          <div class="col-lg-4">
-            <img class="img-thumbnail" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
-            <h4>Heading</h4>
-            <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
-            <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
-          </div><!-- /.col-lg-4 -->
-          <div class="col-lg-4">
-            <img class="img-thumbnail" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
-            <h4>Heading</h4>
-            <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-            <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
-          </div><!-- /.col-lg-4 -->
-        </div><!-- /.row -->
-      </div>
-    </div>
-    </div>
-    </main>
-
-        
-        
-        
-        
-        
-    
-            <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 indexftp">
-                    <center>
-                        <div class="indexft"><a href="http://www.zoothailand.org" target="_blank"><img src="images/Logo/ZPO.png"/></a></div>
-                        <div class="indexft"><a href="http://www.dusitzoo.org" target="_blank"><img src="images/Logo/Dusitzoo.png"></a></div>
-                        <div class="indexft"><a href="http://kkopenzoo.com/newversion_index.php"><img src="images/Logo/KKOzoo.png"></a></div>
-                        <div class="indexft"><a href="http://www.chiangmaizoo.com" target="_blank"><img src="images/Logo/chiangmai.png"></a></div>
-                        <div class="indexft"><a href="http://www.koratzoo.org" target="_blank"><img src="images/Logo/Nakhonrachsimazoo.png"></a></div>
-                        <div class="indexft"><a href="http://www.songkhlazoo.com" target="_blank"><img src="images/Logo/Songkhlazoo.png"></a></div>
-                        <div class="indexft"><a href="http://www.ubon-zoo.com" target="_blank"><img src="images/Logo/Ubonzoo.png"></a></div>
-                        <div class="indexft"><a href="http://www.khonkaenzoo.com" target="_blank"><img src="images/Logo/KKzoo.png"></a></div>
-                        <div class="indexft"><a href="#" target="_blank"><img src="images/Logo/surin.png"></a></div>
-                    </center>
-                </div>
-            </div>
-        </div> <!--end container-->
-        </div> <!--end wrapper-->
-    <footer class="footer">
-      <div class="container">
-        <span class="text-muted">
-            นโยบาย
-        </span>
-      </div>
-    </footer>
+		<!--<footer class="footer">
+			<div class="container">
+				<span class="text-muted">
+				นโยบาย
+				</span>
+			</div>
+		</footer>-->
     </body>
 
 		<script type="text/javascript">
--- menu_main.php
+++ menu_main.php
@@ -8,74 +8,72 @@
 <!--      <a href="km_index.php" class="list-group-item list-group-item-action"><i><img src="images/icons/glyphicons-30-notes-2.png" style="width: 17px; height: 15px;"></i> ระบบบริหารจัดการความรู้</a> -->
  <!--   <a href="trs_index.php" class="list-group-item list-group-item-action"><span data-feather="bar-chart"></span>ระบบรายงานจำนวนผู้เข้าชมสวนสัตว์</a>
 -->
-
-<nav class="col-md-2 d-none d-md-block bg-light sidebar">
-          <div class="sidebar-sticky">
+	<nav class="col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2 d-none d-md-block bg-light sidebar">
+        <div class="sidebar-sticky">
             <ul class="nav flex-column">
-              <li class="nav-item">
-                <a class="nav-link active" href="#">
-                  <span data-feather="home"></span>
-                  หน้าหลัก <span class="sr-only">(current)</span>
-                </a>
-              </li> 
-              <li class="nav-item">
-                <a class="nav-link" href="https://172.16.0.1:4100/logon.shtml?redirect=http://192.168.0.1/">
+				<li class="nav-item">
+					<a class="nav-link active" href="#">
+					<span data-feather="home"></span>
+					หน้าหลัก <span class="sr-only">(current)</span>
+					</a>
+				</li> 
+				<li class="nav-item">
+					<a class="nav-link" href="https://172.16.0.1:4100/logon.shtml?redirect=http://192.168.0.1/">
                     <span data-feather="log-in"></span>
-                  เข้าระบบอินเตอร์เน็ต
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="http://mail.zoothailand.org">
+					เข้าระบบอินเตอร์เน็ต
+					</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="http://mail.zoothailand.org">
                     <span data-feather="mail"></span>
-                อีเมล์
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
+					อีเมล์
+					</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">
                     <span data-feather="database"></span>
-                  ระบบฝากไฟล์
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" id="confer" href="cf_index.php">
-                  <span data-feather="calendar"></span>
-                  ระบบจองห้องประชุม
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" id="cs" href="#">
+					ระบบฝากไฟล์
+					</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" id="confer" href="cf_index.php">
+					<span data-feather="calendar"></span>
+					ระบบจองห้องประชุม
+					</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" id="cs" href="#">
                     <span data-feather="cpu"></span>
-                 ระบบแจ้งซ่อมคอมพิวเตอร์
-                </a>
+					ระบบแจ้งซ่อมคอมพิวเตอร์
+					</a>
                 <!-- sub menu -->
-                <a class="nav-link dropcs ml-4" href="cs_index.php?url=cs_add_problem.php">
-                   แบบฟอร์มแจ้งซ่อม
-                </a>
-                <a class="nav-link dropcs ml-4" href="cs_index.php?url2=cs_show_problem.php&subpage=1">
-                   รายการแจ้งซ่อม
-                </a>
-              <!-- end sub menu -->
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" id="trs" href="#">
+					<a class="nav-link dropcs ml-4" href="cs_index.php?url=cs_add_problem.php">
+						แบบฟอร์มแจ้งซ่อม
+					</a>
+					<a class="nav-link dropcs ml-4" href="cs_index.php?url2=cs_show_problem.php&subpage=1">
+						รายการแจ้งซ่อม
+					</a>
+				<!-- end sub menu -->
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" id="trs" href="#">
                     <span data-feather="bar-chart"></span>
-                  ระบบรายงานจำนวนผู้เข้าชม
-                </a>
+					ระบบรายงานจำนวนผู้เข้าชม
+					</a>
                 <!-- sub menu -->
-                <a class="nav-link droptrs ml-4" href="trs_index.php?url=trs_showallzoo.php">
-                   รายงานจำนวนผู้เข้าชมของสวนสัตว์
-                </a>
-                <a class="nav-link droptrs ml-4" href="trs_index.php?url2=trs_showallzoo_old.php">
-                   รายงานจำนวนผู้เข้าชมของสวนสัตว์แบบเก่า
-                </a>
-                <a class="nav-link droptrs ml-4" href="trs_index.php?url2=trs_showallvehicle.php">
-                   รายงานจำนวนยานพาหนะ
-                </a>
-              <!-- end sub menu -->
-              </li>
+					<a class="nav-link droptrs ml-4" href="trs_index.php?url=trs_showallzoo.php">
+					รายงานจำนวนผู้เข้าชมของสวนสัตว์
+					</a>
+					<a class="nav-link droptrs ml-4" href="trs_index.php?url2=trs_showallzoo_old.php">
+					รายงานจำนวนผู้เข้าชมของสวนสัตว์แบบเก่า
+					</a>
+					<a class="nav-link droptrs ml-4" href="trs_index.php?url2=trs_showallvehicle.php">
+					รายงานจำนวนยานพาหนะ
+					</a>
+				<!-- end sub menu -->
+				</li>
             </ul>
-
-            <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+            <!--<h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
               <span>Saved reports</span>
             </h6>
             <ul class="nav flex-column mb-2">
@@ -103,7 +101,6 @@
                   Year-end sale
                 </a>
               </li>
-            </ul>
-          </div>
-        </nav>
+            </ul>-->
         </div>
+    </nav>
