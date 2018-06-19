a9fc513692f6375421479abf6489b31219cbabd8
choatchaw <choatchaw@gmail.com>
2018-06-19 21:28:01 +0700

    งงต่อไป

--- index.php
+++ index.php
@@ -49,35 +49,7 @@ session_start();
 		<main role="main" class="container">
 			<div class="row">
 				<?php include('menu_main.php'); ?>
-				<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" style="padding-left:150px;">
-					<div class="jumbotron">
-						<h3>ข่าวสารภายในองค์การสวนสัตว์</h3>
-						<div class="container marketing">
-
-						<!-- Three columns of text below the carousel -->
-						<div class="row">
-						  <div class="col-lg-4">
-							<img class="img-thumbnail" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
-							<h4>Heading</h4>
-							<p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
-							<p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
-						  </div><!-- /.col-lg-4 -->
-						  <div class="col-lg-4">
-							<img class="img-thumbnail" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
-							<h4>Heading</h4>
-							<p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
-							<p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
-						  </div><!-- /.col-lg-4 -->
-						  <div class="col-lg-4">
-							<img class="img-thumbnail" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
-							<h4>Heading</h4>
-							<p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-							<p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
-						  </div><!-- /.col-lg-4 -->
-						</div><!-- /.row -->
-						</div>
-					</div>
-				</div>
+				<?php include('content.php'); ?>
 				<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" style="padding-left:350px;">
 					<center>
 						<div class="indexft"><a href="http://www.zoothailand.org" target="_blank"><img src="images/Logo/ZPO.png"/></a></div>
