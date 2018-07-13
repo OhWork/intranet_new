0c34eba5cc7fc3c15edb4671d16d2a0da1809856
Puttikun Wongpanyanurak <puttikun@hotmail.com>
2018-07-13 23:28:29 +0700

    Sync ลองเอาไปใช้ดูก่อน

--- hrs_add_hospital.php
+++ hrs_add_hospital.php
@@ -5,7 +5,7 @@
     $lbposition = new label('ตำแหน่ง');
     $lbdevision = new label('สำนัก/สวน');
     $radioparent = new radioGroup();
-    $radioparent->name = 'systemallow_news';
+    $radioparent->name = 'parent';
     $radioparent->add('ข้าพเจ้า',1,'');
     $radioparent->add('คู่สมรส ชื่อ',2,'');
     $radioparent->add('คู่บิดา ชื่อ',2,'');
@@ -16,6 +16,11 @@
     $lbtime = new label('วันที่และเวลาแจ้ง');
     $lbtypetools = new label('ชนิดของอุปกรณ์');
     $txtname = new textfield('','','form-control','','');
+    $txtyourname = new textfield('','yourname','form-control ','','');
+    $txtfamily = new textfield('','family','form-control ','','');
+    $txtfather = new textfield('','father','form-control ','','');
+    $txtmather = new textfield('','mather','form-control ','','');
+    $txtchild = new textfield('','child','form-control ','','');
     $txttime = new textfieldcalendarreadonly('problem_date','datetimepicker1','','form-control','input-group-addon btn calen','datetimepicker1');
     $year = date("Y")+543;
     $md = date("m-d");
@@ -28,12 +33,94 @@
     $txtdetail->rows = 5;
     $button = new buttonok("ส่งแบบฟอร์มแจ้งซ่อม","","btn btn-success btn-lg btn-block bt3success col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12","");
     ?>
-    
+
   <h4>คำขอหนังสือรับรองการมีสิทธิรับเงินช่วยเหลือค่ารักษาพยาบาล</h4>
   <?php
       echo $form->open("","","","hrs_insert_hospital.php","");
       echo $lbname;
       echo $txtname;
-      echo $radioparent;
-      echo $form->close();
+      ?>
+						<div class="col-md-4">
+							<label class="col-md-12">
+							  <input type="radio" name="parent" value="1" > ข้าพเจ้า
+							  <?php
+								  echo $txtyourname;
+							  ?>
+							</label>
+							<label class=" col-md-12">
+							  <input type="radio" name="parent" value="2"> คู่สมรส
+							  <?php
+								  echo $txtfamily;
+							  ?>
+							</label>
+							<label class="col-md-12">
+							  <input type="radio" name="parent" value="3"> คู่บิดา
+							  <?php
+								  echo $txtfather;
+							  ?>
+							</label>
+							<label class="col-md-12">
+							  <input type="radio" name="parent" value="4"> คู่มารดา
+							  <?php
+								  echo $txtmather;
+							  ?>
+							</label>
+							<label class="col-md-12">
+							  <input type="radio" name="parent" value="5"> คู่บุตร
+							  <?php
+								  echo $txtchild;
+							  ?>
+							</label>
+						</div>
+						<div class='col-md-4'></div>
+      <?
+	    echo $form->close();
   ?>
+  <script>
+  		$(document).ready(function(){
+		$('#yourname').hide();
+		$('#family').hide();
+		$('#father').hide();
+		$('#mather').hide();
+		$('#child').hide();
+		$('input[name=parent]').on("change",function(e) {
+		var valchange = $(this).val();
+			if(valchange == 1){
+				$('#yourname').show("slow");
+				$('#family').hide("slow");
+				$('#father').hide("slow");
+				$('#mather').hide("slow");
+				$('#child').hide("slow");
+			}
+			else if(valchange == 2) {
+				$('#family').show("slow");
+				$('#yourname').hide("slow");
+				$('#father').hide("slow");
+				$('#mather').hide("slow");
+				$('#child').hide("slow");
+			}
+			else if(valchange == 3) {
+				$('#father').show("slow");
+				$('#yourname').hide("slow");
+				$('#family').hide("slow");
+				$('#mather').hide("slow");
+				$('#child').hide("slow");
+			}
+			else if(valchange == 4) {
+				$('#mather').show("slow");
+				$('#yourname').hide("slow");
+				$('#family').hide("slow");
+				$('#father').hide("slow");
+				$('#child').hide("slow");
+			}
+			else if(valchange == 5) {
+				$('#child').show("slow");
+				$('#yourname').hide("slow");
+				$('#family').hide("slow");
+				$('#father').hide("slow");
+				$('#mather').hide("slow");
+			}
+		});
+	});
+
+  </script>
