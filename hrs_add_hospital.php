31917bb971fed644cbc22c28e20d3f0ae5b05539
Firore <takbasic722@gmail.com>
2018-07-18 23:46:08 +0700

    *0*

--- hrs_add_hospital.php
+++ hrs_add_hospital.php
@@ -23,59 +23,136 @@
     $txtchild = new textfield('','child','form-control ','','');
     $txthospital = new textfield('','','form-control','','');
     $txtprovince = new textfield('','','form-control','','');
-    $txtdatestart = new textfieldcalendarreadonly('problem_date','datetimepicker1','','form-control','input-group-addon btn calen','datetimepicker1');
-    $txtdateend = new textfieldcalendarreadonly('problem_date','datetimepicker2','','form-control','input-group-addon btn calen','datetimepicker1');
-    $id = $_GET['id'];
+    $txtdatestart = new textfieldcalendarreadonly('problem_date','datetimepicker1','','form-control','input-group-addon','datetimepicker1');
+    $txtdateend = new textfieldcalendarreadonly('problem_date','datetimepicker2','','form-control','input-group-addon','datetimepicker1');
+    //$id = $_GET['id'];
     $button = new buttonok("ส่งแบบฟอร์ม","","btn btn-success btn-lg btn-block bt3success col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12","");
-    ?>
-
-  <h4>คำขอหนังสือรับรองการมีสิทธิรับเงินช่วยเหลือค่ารักษาพยาบาล</h4>
-  <?php
-      echo $form->open("","","","hrs_insert_hospital.php","");
-      echo $lbname;
-      echo $txtname;
-      ?>
-							<label class="col-md-12">
-							  <input type="radio" name="parent" id="parent1" value="1"> ข้าพเจ้า
-							</label>
-							<label class=" col-md-12">
-							  <input type="radio" name="parent" id="parent2" value="2"> คู่สมรส
-							  <?php
-								  echo $txtfamily;
-							  ?>
-							</label>
-							<label class="col-md-12">
-							  <input type="radio" name="parent"  id="parent3" value="3"> คู่บิดา
-							  <?php
-								  echo $txtfather;
-							  ?>
-							</label>
-							<label class="col-md-12">
-							  <input type="radio" name="parent" id="parent4" value="4"> คู่มารดา
-							  <?php
-								  echo $txtmather;
-							  ?>
-							</label>
-							<label class="col-md-12">
-							  <input type="radio" name="parent" id="parent5" value="5"> คู่บุตร
-							  <?php
-								  echo $txtchild;
-							  ?>
-							</label>
-							<?php
-    							echo $lbhospital;
-    							echo $txthospital;
-    							echo $lbprovince;
-    							echo $txtprovince;
-    							echo $lbdatestart;
-    							echo $txtdatestart;
-    							echo $lbdateend;
-    							echo $txtdateend;
-    							echo $button;
-							?>
-      <?
-	    echo $form->close();
-  ?>
+    echo $form->open("","","col-xl-10 col-lg-10 col-md-10 col-sm-10 col-10","hrs_insert_hospital.php","");
+	?>
+<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" style="padding-top:8px;">
+	<div class="row">
+		<div class="col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2"></div>
+		<div class="col-xl-8 col-lg-8 col-md-8 col-sm-8 col-8">
+			<div class="row">
+				<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+					<h4>คำขอหนังสือรับรองการมีสิทธิรับเงินช่วยเหลือค่ารักษาพยาบาล</h4>
+				</div>
+				<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+					<div class="row">
+						<div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-4">
+							<?php echo $lbname; ?>
+						</div>
+						<div class="col-xl-8 col-lg-8 col-md-8 col-sm-8 col-8">
+							<?php echo $txtname; ?>
+						</div>
+					</div>
+				</div>
+				<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" style="margin-top:8px;">
+					<div class="row">
+						<label class="col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2">
+							<input type="radio" name="parent" id="parent1" value="1"> ข้าพเจ้า
+						</label>
+						<label class="col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2">
+							<input type="radio" name="parent" id="parent2" value="2"> คู่สมรส
+						</label>
+						<label class="col-xl-8 col-lg-8 col-md-8 col-sm-8 col-8">
+							<?php echo $txtfamily; ?>
+						</label>
+					</div>
+				</div>
+				<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+					<div class="row">
+						<label class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-4">
+							<input type="radio" name="parent"  id="parent3" value="3"> คู่บิดา
+						</label>
+						<label class="col-xl-8 col-lg-8 col-md-8 col-sm-8 col-8">
+							<?php echo $txtfather;?>
+						</label>
+					</div>
+				</div>
+				<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+					<div class="row">
+						<label class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-4">
+							<input type="radio" name="parent" id="parent4" value="4"> คู่มารดา
+						</label>
+						<label class="col-xl-8 col-lg-8 col-md-8 col-sm-8 col-8">
+							<?php echo $txtmather;?>
+						</label>
+					</div>
+				</div>
+				<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+					<div class="row">
+						<label class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-4">
+							<input type="radio" name="parent" id="parent5" value="5"> คู่บุตร
+						</label>
+						<label class="col-xl-8 col-lg-8 col-md-8 col-sm-8 col-8">
+							<?php echo $txtchild; ?>
+						</label>
+					</div>
+				</div>
+				<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" style="margin-top:8px;">
+					<div class="row">
+						<div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-4">
+							<?php echo $lbhospital; ?>
+						</div>
+						<div class="col-xl-8 col-lg-8 col-md-8 col-sm-8 col-8">
+							<?php echo $txthospital; ?>
+						</div>
+					</div>
+				</div>
+				<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" style="margin-top:8px;">
+					<div class="row">
+						<div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-4">
+							<?php echo $lbprovince; ?>
+						</div>
+						<div class="col-xl-8 col-lg-8 col-md-8 col-sm-8 col-8">
+							<?php echo $txtprovince; ?>
+						</div>
+					</div>
+				</div>
+				<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" style="margin-top:8px;">
+					<div class="row">
+						<div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-4">
+							<?php echo $lbdatestart; ?>
+						</div>
+						<div class="date-form dayinbox col-xl-4 col-lg-4 col-md-4 col-sm-4 col-4 form-horizontal control-group controls">
+                            <div class="input-group">
+								<?php echo $txtdatestart; ?>
+							</div>
+						</div>
+						<div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-4"></div>
+					</div>
+				</div>
+				<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" style="margin-top:8px;">
+					<div class="row">
+						<div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-4">
+							<?php echo $lbdateend; ?>
+						</div>
+						<div class="date-form dayinbox col-xl-4 col-lg-4 col-md-4 col-sm-4 col-4 form-horizontal control-group controls">
+                            <div class="input-group">
+								<?php echo $txtdateend; ?>
+							</div>
+						</div>
+						<div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-4"></div>
+					</div>
+				</div>
+				<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" style="margin-top:8px;">
+					<div class="row">
+						<div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-4"></div>
+						<div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col-3">
+							<?php echo $button; ?>
+						</div>
+						<div class="col-xl-5 col-lg-5 col-md-5 col-sm-5 col-5"></div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2"></div>
+	</div>
+</div>					
+<?php
+	echo $form->close();
+?>
   <script>
   		$(document).ready(function(){
   		$('#parent1').prop('checked',true);
