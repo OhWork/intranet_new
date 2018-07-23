fa23395da77f08bfc71798ae8eaf85ce44929ead
Puttikun Wongpanyanurak <puttikun@hotmail.com>
2018-07-23 11:22:56 +0700

    น่าจะใช่

--- hrs_add_certificate.php
+++ hrs_add_certificate.php
@@ -14,6 +14,7 @@
     $txtposition = new textfield('problem_position','problem_position','form-control','','');
     $txtdetail = new textarea('problem_detail','aprob','','');
     $txtdetail->rows = 5;
+    $txtsubname = new textfield('','hrhos_subname','form-control ','','');
     $button = new buttonok("ส่งแบบฟอร์ม","","btn btn-success btn-lg btn-block bt3success col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12","");
     echo $form->open("","","col-xl-10 col-lg-10 col-md-10 col-sm-10 col-10","","");
  ?>
@@ -81,6 +82,71 @@
 				</div>
 				<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" style="margin-top:8px;">
 					<div class="row">
+						<label class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-4">
+							<input type="radio" name="hrhos_familytype" id="parent1" value="1"> รับรองเงินเดือน
+						</label>
+						<label class="col-xl-8 col-lg-8 col-md-8 col-sm-8 col-8">
+
+						</label>
+					</div>
+				</div>
+				<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" style="margin-top:8px;">
+					<div class="row">
+						<label class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-4">
+							<input type="radio" name="hrhos_familytype" id="parent2" value="2"> รับรองการเป็นผู้ปฏิบัติงาน
+						</label>
+						<label class="col-xl-8 col-lg-8 col-md-8 col-sm-8 col-8">
+
+						</label>
+					</div>
+				</div>
+
+				<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+					<div class="row">
+						<label class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-4">
+							<input type="radio" name="hrhos_familytype"  id="parent3" value="3"> รับรองเข้ารับการศึกษา (สถาบันการศึกษา)
+						</label>
+						<label class="col-xl-8 col-lg-8 col-md-8 col-sm-8 col-8">
+							<?php echo $txtsubname;?>
+						</label>
+					</div>
+				</div>
+				<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+					<div class="row">
+						<label class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-4">
+							<input type="radio" name="hrhos_familytype"  id="parent4" value="4"> รับรองการคำ้ประกันการกู้เงินจากสถาบันการเงิน
+						</label>
+						<label class="col-xl-8 col-lg-8 col-md-8 col-sm-8 col-8">
+						</label>
+					</div>
+				</div>
+				<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+					<div class="row">
+						<label class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col-3">
+							<input type="radio" name="hrhos_familytype" id="parent5" value="5"> รับรองการคำ้ประกันการเข้าทำงาน ของ
+						</label>
+						<label class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col-3">
+							<?php echo $txtsubname;?>
+						</label>
+						<label class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col-3"> ซึ่งเกี่ยวข้องกับข้าพเจ้าโดยเป็น
+						</label>
+						<label class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col-3">
+							<?php echo $txtsubname;?>
+						</label>
+					</div>
+				</div>
+				<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+					<div class="row">
+						<label class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-4">
+							<input type="radio" name="hrhos_familytype"  id="parent6" value="6"> อื่นๆ
+						</label>
+						<label class="col-xl-8 col-lg-8 col-md-8 col-sm-8 col-8">
+						<?php echo $txtsubname;?>
+						</label>
+					</div>
+				</div>
+				<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" style="margin-top:8px;">
+					<div class="row">
 						<div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-4"></div>
 						<div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col-3">
 							<?php echo $button; ?>
@@ -93,3 +159,39 @@
 		<div class="col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2"></div>
 	</div>
 </div>
+<script>
+  		$(document).ready(function(){
+  		$('#parent1').prop('checked',true);
+  		$('[id=hrhos_subname]').slice(0).prop("disabled", true);
+		$('input[name=hrhos_familytype]').on("change",function(e) {
+		var valchange = $(this).val();
+			if(valchange == 1){
+			$('[id=hrhos_subname]').slice(0).prop("disabled", true);
+			}
+			else if(valchange == 2) {
+			$('[id=hrhos_subname]').slice(0).prop("disabled", true);
+			}
+			else if(valchange == 3) {
+				$('[id=hrhos_subname]').slice(0,1).prop("disabled", false);
+				$('[id=hrhos_subname]').slice(1,2).prop("disabled", true);
+				$('[id=hrhos_subname]').slice(2,3).prop("disabled", true);
+				$('[id=hrhos_subname]').slice(3,4).prop("disabled", true);
+			}
+			else if(valchange == 4) {
+				$('[id=hrhos_subname]').slice(0).prop("disabled", true);
+			}
+			else if(valchange == 5) {
+				$('[id=hrhos_subname]').slice(1,2).prop("disabled", false);
+				$('[id=hrhos_subname]').slice(2,3).prop("disabled", false);
+				$('[id=hrhos_subname]').slice(0,1).prop("disabled", true);
+				$('[id=hrhos_subname]').slice(3,4).prop("disabled", true);
+			}
+			else if(valchange == 6) {
+				$('[id=hrhos_subname]').slice(3,4).prop("disabled", false);
+				$('[id=hrhos_subname]').slice(0,1).prop("disabled", true);
+				$('[id=hrhos_subname]').slice(1,2).prop("disabled", true);
+				$('[id=hrhos_subname]').slice(2,3).prop("disabled", true);
+			}
+		});
+	});
+</script>
