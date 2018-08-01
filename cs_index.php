f6640adc4f0507122bffa889459e39a2d314d84c
choatchaw <choatchaw@gmail.com>
2018-08-01 20:13:59 +0700

    แก้

--- cs_index.php
+++ cs_index.php
@@ -71,66 +71,6 @@ $('#Modal').on('show.bs.modal', function (event) {
             });
     })
 
-// function chk_form(){
-// 	$(":input + span.require").remove();
-// 	$(":input").each(function(){
-// 		$(this).each(function(){
-// 			if($(this).val()==""){
-// 				$(this).after("<span class=require>« จำเป็นต้องกรอก</span>");
-// 			}
-// 		});
-// 	});
-// 	if($(":input").next().is(".require")==false){
-// 		return true;
-// 	}else{
-// 		return false;
-// 	}
-// }
-
-/*
- $(function(){
-
-     var obj_check=$(".css-require");
-     $("#myform1").on("submit",function(){
-         obj_check.each(function(i,k){
-             var status_check=0;
-             if(obj_check.eq(i).find(":radio").length>0 || obj_check.eq(i).find(":checkbox").length>0){
-                 status_check=(obj_check.eq(i).find(":checked").length==0)?0:1;
-             }else{
-                 status_check=($.trim(obj_check.eq(i).val())=="")?0:1;
-             }
-             formCheckStatus($(this),status_check);
-         });
-         if($(this).find(".has-error").length>0){
-              return false;
-         }
-     });
-
-     obj_check.on("change",function(){
-         var status_check=0;
-         if($(this).find(":radio").length>0 || $(this).find(":checkbox").length>0){
-             status_check=($(this).find(":checked").length==0)?0:1;
-         }else{
-             status_check=($.trim($(this).val())=="")?0:1;
-         }
-         formCheckStatus($(this),status_check);
-     });
-
-     var formCheckStatus = function(obj,status){
-         if(status==1){
-             obj.parent(".form-group").removeClass("has-error").addClass("has-success");
-             obj.next(".glyphicon").removeClass("glyphicon-warning-sign").addClass("glyphicon-ok");
-         }else{
-             obj.parent(".form-group").removeClass("has-success").addClass("has-error");
-             obj.next(".glyphicon").removeClass("glyphicon-ok").addClass("glyphicon-warning-sign");
-         }
-     }
- });
-
-*/
-
-
-
 </script>
 
 </html>
--- index.php
+++ index.php
@@ -87,14 +87,16 @@ session_start();
 
 		<script type="text/javascript">
     //Modal
+//Modal
 $('#Modal').on('show.bs.modal', function (event) {
           var button = $(event.relatedTarget) // Button that triggered the modal
           var recipient = button.data('whatever') // Extract info from data-* attributes
           var modal = $(this);
           var dataString = 'id=' + recipient;
+
             $.ajax({
                 type: "GET",
-                url: "news_getnewsdetail.php",
+                url: "cs_getdetail.php",
                 data: dataString,
                 cache: false,
                 success: function (data) {
