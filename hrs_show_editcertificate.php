34e0ad767277fcf59aa7b4d9cda53e719f7e520e
Firore <takbasic722@gmail.com>
2018-08-07 23:29:47 +0700

    up up up

--- hrs_show_editcertificate.php
+++ hrs_show_editcertificate.php
@@ -15,21 +15,30 @@
     } );
 } );
 </script>
-
-    <div class='col-md-12' style="padding-top:16px;"><h4>รายการหนังสือรับรอง</h4></div>
-
-<?php
-    $columns = array('typectf_name','hrctf_name','hrctf_position','zoo_name');
-    $rs = $db->findByPK33('hrctf','typectf','zoo','typectf_typectf_id','typectf_id','zoo_zoo_id','zoo_id','hrctf_status',"'S'")->execute();
-
-
-			$grid = new gridView();
-			$grid->pr = 'hrctf_id';
-			$grid->header = array('<b><center>หนังสือรับรอง</center></b>','<b><center>ชื่อ-นามสกุล</center></b>','<b><center>ตำแหน่ง</center></b>','<b><center>สังกัด</center></b>','<b><center>#</center></b>');
-			$grid->width = array('30%','20%','15%','15%','20%');
-			$grid->edit = 'admin_index.php?url=hrs_status.php';
-			$grid->name = 'table';
-			$grid->edittxt ='สถานะ';
-			$grid->renderFromDB($columns,$rs);
-   ?> 
-
+<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 mt-3">
+	<div class='row'>
+		<div class='col-xl-1col-lg-1 col-md-1 col-sm-1 col-1'></div>
+		<div class='col-xl-10 col-lg-10 col-md-10 col-sm-10 col-10'>
+			<div class='row'>
+				<div class='col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12'>
+					<h4>รายการหนังสือรับรอง</h4>
+				</div>
+				<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+					<?php
+						$columns = array('typectf_name','hrctf_name','hrctf_position','zoo_name');
+						$rs = $db->findByPK33('hrctf','typectf','zoo','typectf_typectf_id','typectf_id','zoo_zoo_id','zoo_id','hrctf_status',"'S'")->execute();
+								$grid = new gridView();
+								$grid->pr = 'hrctf_id';
+								$grid->header = array('<b><center>หนังสือรับรอง</center></b>','<b><center>ชื่อ-นามสกุล</center></b>','<b><center>ตำแหน่ง</center></b>','<b><center>สังกัด</center></b>','<b><center>#</center></b>');
+								$grid->width = array('30%','20%','15%','15%','20%');
+								$grid->edit = 'admin_index.php?url=hrs_status.php';
+								$grid->name = 'table';
+								$grid->edittxt ='สถานะ';
+								$grid->renderFromDB($columns,$rs);
+					?>
+				</div>
+			</div>
+		</div>
+		<div class='col-xl-1col-lg-1 col-md-1 col-sm-1 col-1'></div>
+	</div>
+</div>
