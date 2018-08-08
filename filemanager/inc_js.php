a3c070c6083c5a91da0cc191da01ca90a062167d
Puttikun Wongpanyanurak <puttikun@hotmail.com>
2018-08-08 11:05:29 +0700

    filemanager

--- filemanager/inc_js.php
+++ filemanager/inc_js.php
@@ -2,4 +2,5 @@
 <script src="../jquery/jquery-3.3.1.min.js"></script>
 <script src="../jquery/popper.js"></script>
 <script src="../jquery/bootstrap.js"></script>
+<script src="../jquery/feather-icon/feather.min.js"></script>
 
