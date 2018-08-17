b574027a013f4a967d4a8d5cd3f1b8ed14c096d8
choatchaw <choatchaw@gmail.com>
2018-08-17 18:05:29 +0700

    ระบบใหม่

---
+++ qtn_add_question.php
@@ -0,0 +1,96 @@
+<?php
+    $form = new form();
+    $lbname = new label('ชื่อแบบสอบถาม : ');
+    $lbdatestart = new label('วันเริ่ม : ');
+    $lbdateend = new label('วันสิ้นสุด : ');
+    $lblink = new label('ลิ้ง : ');
+    $txtname = new textfield('qtn_name','','form-control','','');
+    $txtcolor = new textfield('qtn_color','','form-control','','');
+    $txtlink = new textfield('qtn_link','','form-control','','');
+    $datetime = date("Y-m-d H:i");
+    $button = new buttonok("ส่งแบบสอบถาม","","btn btn-success btn-lg btn-block bt3success col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12","");
+      if(!empty($_GET['user_id'])){
+	$id = $_GET['user_id'];
+	$r = $db->findByPK('user','user_id',$id)->executeRow();
+	$txtname= $r['user_name'];
+	$txtlast= $r['user_last'];
+	}
+    if(!empty($_GET['id'])){
+    $id = $_GET['id'];
+    $r = $db->findByPK('news','news_id',$id)->executeRow();
+    $txtname->value = $r['news_head'];
+    $txtdetail->value = $r['news_detail'];
+    $txttime->value = $r['news_date'];
+    $user_id = $r['user_user_id'];
+    $r2 = $db->findByPK('user','user_id',$user_id)->executeRow();
+    $txtname= $r2['user_name'];
+	$txtlast= $r2['user_last'];
+    }
+	echo '<div class="newaddbox">';
+    echo $form->open("form_reg","form","","qtn_insert_question.php","");
+    echo $lbname.$txtname;
+	echo $lbdatestart;
+	?>
+	<div class='date-form dayinbox col-md-12 form-horizontal control-group controls input-group'>
+									<div class='input-group date' id ="datetimepicker1">
+										<input type='text' class="form-control datetimepicker " name="eventconfer_start" id='date1' readonly/>
+											<span class="input-group-addon">
+												<span class="glyphicon glyphicon-calendar"></span>
+											</span>
+									</div>
+								</div>
+    <?php
+	echo $lbdateend;
+	?>
+	<div class='date-form dayinbox col-md-12 form-horizontal control-group controls input-group'>
+									<div class='input-group date' id ="datetimepicker2">
+										<input type='text' class="form-control datetimepicker" name="eventconfer_end" id='date2' readonly/>
+											<span class="input-group-addon datetimepicker-addon">
+												<span class="glyphicon glyphicon-calendar"></span>
+											</span>
+									</div>
+								</div>
+	<?php
+	echo $lbcolor.$txtcolor;
+	echo $lblink.$txtlink;
+
+   if(!empty($_GET['user_id'])){
+    echo "<input type='hidden' name='user_user_id' value='$_GET[user_id]'/>";
+    }
+   if(!empty($user_id)){
+    echo "<input type='hidden' name='user_user_id' value='$user_id'/>";
+     }
+     echo "<input type='hidden' name='qtn_datereg' value='$datetime'/>";
+    echo "<input type='hidden' name='id' value='$_GET[id]'/>";
+    echo "<div class='newaddok'>".$button."</div>";
+    echo $form->close();
+	echo '</div>';
+?>
+<script>
+	 $(function () {
+        $('#datetimepicker1').datetimepicker({
+	        format:'YYYY-MM-DD',
+	        useCurrent: false,
+	        ignoreReadonly: true,
+            sideBySide: true,
+            allowInputToggle: true,
+	        locale:moment.locale('th'),
+	        stepping: 30
+        });
+        $('#datetimepicker2').datetimepicker({
+	        format:'YYYY-MM-DD',
+            useCurrent: false,
+            ignoreReadonly: true,
+            sideBySide: true,
+            allowInputToggle: true,
+            locale:moment.locale('th'),
+            stepping: 30
+        });
+        $("#datetimepicker1").on("dp.change", function (e) {
+            $('#datetimepicker2').data("DateTimePicker").minDate(e.date);
+        });
+        $("#datetimepicker2").on("dp.change", function (e) {
+            $('#datetimepicker1').data("DateTimePicker").maxDate(e.date);
+        });
+    });
+</script>
