3730c0f920e425409a1f89b7f9067fca0e9c8f0a
Puttikun Wongpanyanurak <puttikun@hotmail.com>
2018-08-18 00:18:26 +0700

    อะ จบจ้า

---
+++ admin_qtn_menu.php
@@ -0,0 +1,16 @@
+            <li data-toggle="collapse" data-target="#qtn" class="collapsed">
+                <a class="nav-link" href="#">
+                  <span data-feather="database"></span>
+                  ระบบแบบสำรวจ
+                </a>
+              <!-- sub menu -->
+              <ul class="sub-menu collapse" id="qtn">
+	                <a class="nav-link" href="admin_index.php?url=qtn_showreport.php">
+	                  จัดการแบบสำรวจ
+	                </a>
+	                <a class="nav-link" href="#">
+	                  ตั้งค่า
+	                </a>
+              </ul>
+              <!-- end sub menu -->
+              </li>
