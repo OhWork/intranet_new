509692cec74bf0c8bbfc2497a489c03fb9c90d1b
Firore <takbasic722@gmail.com>
2018-08-21 23:21:19 +0700

    up up

--- news.php
+++ news.php
@@ -55,9 +55,11 @@
 			<div class="col-xl-5 col-lg-5 col-md-5 col-sm-5 col-5 shadow" style="background-color:#ffffff;border-right:5px solid #f5f5f5;">
 				<div class='row'>
 					<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-						<center><img height="250" src='images/Logo/ZPO.png' /></center>
+						<div class='row'>
+							<img height="300" width="100%" src='images/view2.jpg' />
+						</div>
 					</div>
-					<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+					<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 mt-2">
 						<b>How Did van Gogh’s Turbulent Mind Depict One of the Most</b>
 					</div>
 					<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" style="color:#8d8d8d;">
@@ -69,16 +71,18 @@
 				<div class='row'>
 					<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 shadow" style="background-color:#ffffff;">
 						<div class='row'>
-							<div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-4">
-								<center><img style="height:100px;width:100px;" src='images/Logo/ZPO.png' /></center>
+							<div class="col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2" style="margin-right:10px;">
+								<div class='row'>
+									<img height="100" src='images/view.jpg' />
+								</div>
 							</div>
 							<div class="col-xl-8 col-lg-8 col-md-8 col-sm-8 col-8">
 								<div class='row'>
-									<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-										<b>How Did van Gogh’s Turbulent Mind Depict One of the Most</b>
+									<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 mt-3">
+										<a href="#" style="color:#000000;">How Did van Gogh’s Turbulent Mind Depict One of the Most</a>
 									</div>
 									<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" style="color:#8d8d8d;">
-										choatchaw on mar 29,2018 at 12:00 am
+										choatchaw on aug 29,2018 at 12:00 am
 									</div>
 								</div>
 							</div>
@@ -86,16 +90,18 @@
 					</div>
 					<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 shadow mt-2" style="background-color:#ffffff;">
 						<div class='row'>
-							<div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-4">
-								<center><img height="100" src='images/Logo/ZPO.png' /></center>
+							<div class="col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2" style="margin-right:10px;">
+								<div class='row'>
+									<img height="100" src='images/view.jpg' />
+								</div>
 							</div>
 							<div class="col-xl-8 col-lg-8 col-md-8 col-sm-8 col-8">
 								<div class='row'>
-									<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-										<b>How Did van Gogh’s Turbulent Mind Depict One of the Most</b>
+									<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 mt-3">
+										<a href="#" style="color:#000000;">How Did van Gogh’s Turbulent Mind Depict One of the Most</a>
 									</div>
 									<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" style="color:#8d8d8d;">
-										choatchaw on mar 29,2018 at 12:00 am
+										choatchaw on aug 29,2018 at 12:00 am
 									</div>
 								</div>
 							</div>
@@ -103,16 +109,18 @@
 					</div>
 					<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 shadow mt-2" style="background-color:#ffffff;">
 						<div class='row'>
-							<div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-4">
-								<center><img height="100" src='images/Logo/ZPO.png' /></center>
+							<div class="col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2" style="margin-right:10px;">
+								<div class='row'>
+									<img height="100" src='images/view.jpg' />
+								</div>
 							</div>
 							<div class="col-xl-8 col-lg-8 col-md-8 col-sm-8 col-8">
 								<div class='row'>
-									<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-										<b>How Did van Gogh’s Turbulent Mind Depict One of the Most</b>
+									<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 mt-3">
+										<a href="#" style="color:#000000;">How Did van Gogh’s Turbulent Mind Depict One of the Most</a>
 									</div>
 									<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" style="color:#8d8d8d;">
-										choatchaw on mar 29,2018 at 12:00 am
+										choatchaw on aug 29,2018 at 12:00 am
 									</div>
 								</div>
 							</div>
@@ -120,16 +128,18 @@
 					</div>
 					<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 shadow mt-2" style="background-color:#ffffff;">
 						<div class='row'>
-							<div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-4">
-								<center><img height="100" src='images/Logo/ZPO.png' /></center>
+							<div class="col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2" style="margin-right:10px;">
+								<div class='row'>
+									<img height="100" src='images/view.jpg' />
+								</div>
 							</div>
 							<div class="col-xl-8 col-lg-8 col-md-8 col-sm-8 col-8">
 								<div class='row'>
-									<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-										<b>How Did van Gogh’s Turbulent Mind Depict One of the Most</b>
+									<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 mt-3">
+										<a href="#" style="color:#000000;">How Did van Gogh’s Turbulent Mind Depict One of the Most</a>
 									</div>
 									<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" style="color:#8d8d8d;">
-										choatchaw on mar 29,2018 at 12:00 am
+										choatchaw on aug 29,2018 at 12:00 am
 									</div>
 								</div>
 							</div>
