250d4046a038b3b6beec59ab19e4b20226e07ae2
choatchaw <choatchaw@gmail.com>
2018-08-21 11:03:24 +0700

    ลบ

--- admin_qtn_menu.php
+++ admin_qtn_menu.php
@@ -2,5 +2,5 @@
     <!-- sub menu -->
     <ul class="sub-menu collapse mmpd" id="qtn">
 	    <a class="nav-link collapsed py-1" href="admin_index.php?url=qtn_show_question.php">จัดการแบบสอบถาม</a>
-	    <a class="nav-link collapsed py-1" href="#">ตั้งค่า</a>
+<!-- 	    <a class="nav-link collapsed py-1" href="#">ตั้งค่า</a> -->
     </ul>
