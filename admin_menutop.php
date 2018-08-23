2a4d52365abaefa718127504b9eb3f72d15560f6
Firore <takbasic722@gmail.com>
2018-08-23 21:39:21 +0700

    up

--- admin_menutop.php
+++ admin_menutop.php
@@ -1,9 +1,14 @@
 <nav class="navbar navbar-expand-md navbar-dark bg-dark col-md-12">
-    <a class="navbar-brand brandedit" href="#"><h4>Intranet</h4></a>
-    <p class="navbar-nav px-3 text-white col"><?php echo $head; ?></p>
-        <a style="text-align:right;color:#ffffff;">
+    <a class="navbar-brand brandedit" href="#" style="margin-right:90%;">
+		<h4>Intranet</h4>
+	</a>
+	<div class="dropdown">
+		<button class="btn btn-primary rounded-circle" style="padding-left:8px;padding-right:8px;" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
 			<span data-feather="users"></span>
-            <?php echo "คุณ".$_SESSION['user_name']." ".$_SESSION['user_last'];
-            ?></a>
-        <a class="nav-link" href="logout.php"><span data-feather="log-out"></span></a>
+		</button>
+		<div class="dropdown-menu" aria-labelledby="dropdownMenuButton" style="left:-130px">
+			<a class="dropdown-item" href="#" style="border-bottom:1px solid #000000;"><?php echo "คุณ".$_SESSION['user_name']." ".$_SESSION['user_last'];?></a>
+			<a class="dropdown-item" href="logout.php" style="text-align:right;color:red;">Logout <span data-feather="log-out"></span></a>
+		</div>
+	</div>
 </nav>
