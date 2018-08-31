f908d2a4c24ffe72a1aaca492abd9895bf011ce6
choatchaw <choatchaw@gmail.com>
2018-08-31 15:09:08 +0700

    s

--- news_insert_news.php
+++ news_insert_news.php
@@ -21,37 +21,26 @@
 
     $target_dir = 'temp/';
     $target_file = $target_dir.basename($_FILES['news_cover']['name']);
-<<<<<<< HEAD
     $target_dir_save = 'images/news/'.basename($_FILES['news_cover']['name']);
     move_uploaded_file($_FILES['news_cover']['tmp_name'], $target_dir_save);	
-    	
+    
 	$rs = $db->insert('news',array(
 	'news_head' => $_POST['news_head'],
 	'news_datestart' => $_POST['news_datestart'],
 	'news_dateend' => $_POST['news_dateend'],
-=======
-    $target_dir_save = 'images/news/typeDesign/'.basename($_FILES['news_cover']['name']);
-    move_uploaded_file($_FILES['news_cover']['tmp_name'], $target_dir_save);
-
-	$rs = $db->insert('news',array(
-	'news_head' => $_POST['news_head'],
-/*
-	'newsdatestart' => $_POST['newsdatestart'],
-	'newsdateend' => $_POST['newsdateend'],
->>>>>>> 5ec133f5686b76b6ec72f255f25e82bdd70220ed
 	'news_cover' => basename($_FILES['news_cover']['name']),
 	'typeNews_typeNews_id' => $_POST['typeNews_typeNews_id'],
 	'typeDesignnews_id' => $_POST['typeDesignnews_id'],
 	'user_user_id' => $_POST['user_user_id']
-*/
 	));
 	}
 	if($rs || $rsfix){
+
     	if($rs){
     	    echo "<div class='statusok'>เพิ่มสำเร็จ</div>";
-    	    if($rs['typeNews_typeNews_id'] == 1 ){
-    	    $link = "index.php?url=news_add_news_designtype1.php";
-    	    }else if($rs['typeNews_typeNews_id'] == 2 ){
+    	    if($_POST['typeDesignnews_id'] == 1 ){
+    	    $link = "index.php?url=news_add_news/designtype1.php";
+    	    }else if($_POST['typeDesignnews_id'] == 2 ){
             $link = "index.php?url=news_add_news_designtype2.php";
     	    }
     	}else if($rsfix){
