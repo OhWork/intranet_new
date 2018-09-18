e69895350adc22ab2e5c8b9f8120fc1efde7526a
Puttikun Wongpanyanurak <puttikun@hotmail.com>
2018-09-18 14:26:30 +0700

    ลองเอาไปดูก่อนเดะจะตาม แก้ Css ให้ มันโยกไปใส่ไว้ใน main ไม่ได้

--- news_insert_news.php
+++ news_insert_news.php
@@ -8,7 +8,7 @@
 <?php
     include 'database/db_tools.php';
 	include 'connect.php';
-
+	print_r($_POST);
 	if(!empty($_POST['id'])){
 
 		$data['news_head'] = $_POST['news_head'];
@@ -22,28 +22,31 @@
     $target_dir = 'temp/';
     $target_file = $target_dir.basename($_FILES['news_cover']['name']);
     $target_dir_save = 'images/news/'.basename($_FILES['news_cover']['name']);
-    move_uploaded_file($_FILES['news_cover']['tmp_name'], $target_dir_save);	
-    
+    move_uploaded_file($_FILES['news_cover']['tmp_name'], $target_dir_save);
+
 	$rs = $db->insert('news',array(
 	'news_head' => $_POST['news_head'],
-	'news_datestart' => $_POST['news_datestart'],
-	'news_dateend' => $_POST['news_dateend'],
-	'news_cover' => basename($_FILES['news_cover']['name']),
+/*
+	'news_datestart' => $_POST['newsdatestart'],
+	'news_dateend' => $_POST['newsdateend'],
+*/
+// 	'news_cover' => basename($_FILES['news_cover']['name']),
 	'typeNews_typeNews_id' => $_POST['typeNews_typeNews_id'],
-	'typeDesignnews_id' => $_POST['typeDesignnews_id'],
+	'typeDesignnews_typeDesignnews_id' => $_POST['typeDesignnews_id'],
 	'user_user_id' => $_POST['user_user_id']
 	));
 	}
-	if($rs || $rsfix){
+	if(@$rs || @$rsfix){
 
-    	if($rs){
+    	if(@$rs){
+	    	echo 'สำเร็จจ้า' ;
     	    echo "<div class='statusok'>เพิ่มสำเร็จ</div>";
     	    if($_POST['typeDesignnews_id'] == 1 ){
     	    $link = "index.php?url=news_designtype1.php";
     	    }else if($_POST['typeDesignnews_id'] == 2 ){
             $link = "index.php?url=news_designtype2.php";
     	    }
-    	}else if($rsfix){
+    	}else if(@$rsfix){
             echo "<div class='statusok'>แก้ไขสำเร็จ</div>";
         }
 //             $link = "url=admin_index.php?url=admin_news_index.php&user_id=".$_POST['user_user_id'];
