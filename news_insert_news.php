1bdf5fd7ae4b5001d72c35b4a54cc4ce4c545eac
Puttikun Wongpanyanurak <puttikun@hotmail.com>
2018-09-19 20:37:51 +0700

    เอา process bar ออก

--- news_insert_news.php
+++ news_insert_news.php
@@ -8,7 +8,6 @@
 <?php
     include 'database/db_tools.php';
 	include 'connect.php';
-	include 'inc_js.php';
 	if(!empty($_POST['id'])){
 
 		$data['news_head'] = $_POST['news_head'];
@@ -46,16 +45,7 @@
 	if(@$rs || @$rsfix){
 
     	if(@$rs){
-	    	?>
-			<div class="progress">
-			  <div id="dynamic" class="progress-bar progress-bar-success progress-bar-striped active" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%">
-			    <span id="current-progress"></span>
-			  </div>
-			</div>
-			<input id="typedesignew" type="hidden" value="<?php echo $_POST['typeDesignnews_id'];?>">
-  <?php
-/*
-    	    if($_POST['typeDesignnews_id'] == 1 ){
+	      	    if($_POST['typeDesignnews_id'] == 1 ){
     	    $link = "index.php?url=news_designtype1.php";
     	    }else if($_POST['typeDesignnews_id'] == 2 ){
             $link = "index.php?url=news_designtype2.php";
@@ -65,30 +55,6 @@
         }
 //             $link = "url=admin_index.php?url=admin_news_index.php&user_id=".$_POST['user_user_id'];
             header( "Refresh: 200; $link" );
-*/
-}
 }
 ob_end_flush();
 ?>
-<script>
-	$(function() {
-  var current_progress = 0;
-  var interval = setInterval(function() {
-      current_progress += 20;
-      $("#dynamic")
-      .css("width", current_progress + "%")
-      .attr("aria-valuenow", current_progress)
-      .text(current_progress + "% Complete");
-      if (current_progress == 100){
-          clearInterval(interval);
-          var Typedesignnews = $('#typedesignew').val();
-        if(Typedesignnews == 1 ){
-			window.location.href = 'index.php?url=news_designtype1.php';
-        }
-        else if(Typedesignnews == 2){
-	        window.location.href = 'index.php?url=news_designtype/.php';
-        }
-      }
-  }, 500);
-});
-</script>
