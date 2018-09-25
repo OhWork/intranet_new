c8deb94d3507d0301f99fd49c217fe3a2a2b48d4
choatchaw <choatchaw@gmail.com>
2018-09-25 20:12:11 +0700

    แก้

--- news.php
+++ news.php
@@ -1,12 +1,10 @@
 <?php
  			$rs = $db->findByPKDESCLimit21('news','user','user_user_id','user_id','news_id',4)->execute();
 //  			for($i=0;$i<4;$i++){
-			foreach($rs as $show){
-     			echo "<pre>";
-     			print_r($show);
-     			echo "</pre>";
+			
+     			
 
-      		}
+      		
 ?>
 <div class='col-xl-1col-lg-1 col-md-1 col-sm-1 col-1'></div>
 <div class='col-xl-10 col-lg-10 col-md-10 col-sm-10 col-10'>
@@ -15,22 +13,7 @@
 		<h4>ข่าวสารภายในองค์การสวนสัตว์</h4>
 	</div>
    	<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 mt-1">
-		<div class='row'>
-			<div class="col-xl-5 col-lg-5 col-md-5 col-sm-5 col-5 shadow" style="background-color:#ffffff;border-right:5px solid #f5f5f5;">
-				<div class='row'>
-					<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-						<div class='row'>
-							<img height="300" width="100%" src='images/view2.jpg' />
-						</div>
-					</div>
-					<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 mt-2">
-						<b>How Did van Gogh’s Turbulent Mind Depict One of the Most</b>
-					</div>
-					<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" style="color:#8d8d8d;">
-						choatchaw on mar 29,2018 at 12:00 am
-					</div>
-				</div>
-			</div>
+        <?php foreach($rs as $show){ ?>
 			<div class="col-xl-7 col-lg-7 col-md-7 col-sm-7 col-7">
 				<div class='row'>
 					<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 shadow" style="background-color:#ffffff;">
@@ -43,64 +26,7 @@
 							<div class="col-xl-8 col-lg-8 col-md-8 col-sm-8 col-8">
 								<div class='row'>
 									<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 mt-3">
-										<a href="#" style="color:#000000;">How Did van Gogh’s Turbulent Mind Depict One of the Most</a>
-									</div>
-									<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" style="color:#8d8d8d;">
-										choatchaw on aug 29,2018 at 12:00 am
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 shadow mt-2" style="background-color:#ffffff;">
-						<div class='row'>
-							<div class="col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2" style="margin-right:10px;">
-								<div class='row'>
-									<img height="100" src='images/view.jpg' />
-								</div>
-							</div>
-							<div class="col-xl-8 col-lg-8 col-md-8 col-sm-8 col-8">
-								<div class='row'>
-									<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 mt-3">
-										<a href="#" style="color:#000000;">How Did van Gogh’s Turbulent Mind Depict One of the Most</a>
-									</div>
-									<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" style="color:#8d8d8d;">
-										choatchaw on aug 29,2018 at 12:00 am
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 shadow mt-2" style="background-color:#ffffff;">
-						<div class='row'>
-							<div class="col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2" style="margin-right:10px;">
-								<div class='row'>
-									<img height="100" src='images/view.jpg' />
-								</div>
-							</div>
-							<div class="col-xl-8 col-lg-8 col-md-8 col-sm-8 col-8">
-								<div class='row'>
-									<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 mt-3">
-										<a href="#" style="color:#000000;">How Did van Gogh’s Turbulent Mind Depict One of the Most</a>
-									</div>
-									<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" style="color:#8d8d8d;">
-										choatchaw on aug 29,2018 at 12:00 am
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 shadow mt-2" style="background-color:#ffffff;">
-						<div class='row'>
-							<div class="col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2" style="margin-right:10px;">
-								<div class='row'>
-									<img height="100" src='images/view.jpg' />
-								</div>
-							</div>
-							<div class="col-xl-8 col-lg-8 col-md-8 col-sm-8 col-8">
-								<div class='row'>
-									<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 mt-3">
-										<a href="#" style="color:#000000;">How Did van Gogh’s Turbulent Mind Depict One of the Most</a>
+										<a href="#" style="color:#000000;"><?php echo $show['news_head']; ?></a>
 									</div>
 									<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" style="color:#8d8d8d;">
 										choatchaw on aug 29,2018 at 12:00 am
@@ -110,9 +36,14 @@
 						</div>
 					</div>
 				</div>
-			</div>
-		</div>
-	</div>
+			</div>	
+<?php } ?>
+				
+            
+        </div>
+    </div>
+    
+</div>
 </div>
 </div>
 <div class='col-xl-1col-lg-1 col-md-1 col-sm-1 col-1'></div>
