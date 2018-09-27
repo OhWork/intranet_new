fd71eb5047ae79a6ab311f734ea7a3c4394ae171
Puttikun Wongpanyanurak <puttikun@hotmail.com>
2018-09-27 21:33:14 +0700

    New 5 แบบสามารถแอดได้หมดแล้ว ส่วนเรื่องบัคเดียวจะเช็คให้ พน วันนี้ไม่ไหวแล้ววว

--- news_designtype5.php
+++ news_designtype5.php
@@ -1,12 +1,8 @@
-<?php
-    $id = $_GET['id'];
-    $rs = $db->findByPK55('news','newsDetails','newsVideo','newsImg','user','news_newsDetails_id','newsDetails_id','news_newsImg_id','newsImg_id','news_newsVideo_id','newsVideo_id','user_user_id','user_id','news_id',$id)->executeAssoc();
-?>
 <div class='col-xl-1 col-lg-1 col-md-1 col-sm-1 col-1'></div>
 <div class='col-xl-10 col-lg-10 col-md-10 col-sm-10 col-10' style="background-color:#ffffff;">
 	<div class='row'>
 		<div class='col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 mt-3'>
-			<h3> <?php echo $rs['news_head'];?> </h3>
+			<h3> HEAD NEWS </h3>
 		</div>
 		<div class='col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 mt-3'>
 			<div class='row'>
@@ -14,23 +10,23 @@
 				<div class='col-xl-8 col-lg-8 col-md-8 col-sm-8 col-8'>
 					<div class='row'>
 						<div class='col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 mt-3' style="color:#007bff;">
-						<?php
-								echo 'เพิ่มข่าวสารโดยคุณ ',$rs['user_name'],' ',$rs['user_last'],' ',$rs['news_dateupdate'];
-							?>
+							post by choatchaw 22 aug 2561 10.00am
 						</div>
 						<div class='col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12'>
 							<div class='row'>
 								<div class='col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2'></div>
 								<div class='col-xl-8 col-lg-8 col-md-8 col-sm-8 col-8'>
-									<video width="320" height="240" controls>
-										<source src="<?php echo $rs['newsVideo_link'],$rs['newsVideo_name'] ;?>" type="video/mp4">
-								  	</video>
+									<img height="250" width="100%" src='images/vdo.jpg' />
 								</div>
 								<div class='col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2'></div>
 							</div>
 						</div>
 						<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 mt-2">
-							<?php echo $rs['newsDetails_name'];?>
+							How Did van Gogh’s Turbulent Mind Depict One of the Most How Did van Gogh’s Turbulent Mind Depict One 
+							of the Most How Did van Gogh’s Turbulent Mind Depict One of the Most How Did van Gogh’s Turbulent Mind
+							Depict One of the Most How Did van Gogh’s Turbulent Mind Depict One of the Most How Did van Gogh’s 
+							Turbulent Mind Depict One of the Most How Did van Gogh’s Turbulent Mind Depict One of the Most How 
+							Did van Gogh’s Turbulent Mind Depict One of the Most
 						</div>
 					</div>
 				</div>
@@ -39,15 +35,25 @@
 		</div>
 		<div class='col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 mt-3 mb-3'>
 			<div class='row'>
-				<?php
-					$rsimg = $db->findByPK12('newsImg','newsImg_position',2,'newsImg_connect',$id)->execute();
-					foreach($rsimg as $showimg){
-				?>
 				<div class='col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2'>
-					<img height="100" width="100%" src='<?php echo $showimg['newsImg_path'],$showimg['newsImg_name'] ; ?>' />
+					<img height="100" width="100%" src='images/view.jpg' />
 				</div>
-				<?php  } ?>
+				<div class='col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2'>
+					<img height="100" width="100%" src='images/view2.jpg' />
 				</div>
+				<div class='col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2'>
+					<img height="100" width="100%" src='images/view.jpg' />
+				</div>
+				<div class='col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2'>
+					<img height="100" width="100%" src='images/view2.jpg' />
+				</div>
+				<div class='col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2'>
+					<img height="100" width="100%" src='images/view.jpg' />
+				</div>
+				<div class='col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2'>
+					<img height="100" width="100%" src='images/view2.jpg' />
+				</div>
+			</div>
 		</div>
 	</div>
 </div>
--- news_formdesign4.php
+++ news_formdesign4.php
@@ -1,39 +1,21 @@
 <?php
-	$datetime = date("Y-m-d H:i");
 	$form = new form();
 	$lbdetailnews = new label('รายละเอียด');
-	$lbheadnews = new label('หัวข้อข่าวสาร');
-	$txtheadnews = new textfield('news_head','','form-control','','');
-	$lbpic = new label('ภาพ');
 	$lbvdo = new label('วีดีโอ');
-	$filevideo = new inputFile('news_videodetail','','file_id');
-	$filepic = new inputFile('news_picdetail[]','','file_id');
-	$detailnews = new textAreareadonly('detail_news[]','form-control','text_editer','','5','5','');
-	$detailnews2 = new textAreareadonly('detail_news[]','form-control','text_editer2','','5','5','');
+	$lbpic = new label('ภาพ');
+	$filepic = new inputFile('news_detail','','');
+	$detailnews = new textAreareadonly('detail_news','form-control','text_editer','','5','5','หากต้องการใส่รายละเอียดกรุณาคลิก');
 	if(!empty($_GET['id'])){
-		$id=$_GET['id'];
-		$r2 = $db->findByPK('news','news_id',$id)->executeRow();
-		$txtheadnews->value = $r2['news_head'];
-		$r = $db->findByPK('newsDetails','newsDetails_id',$r2['news_newsDetails_id'])->executeRow();
-		if($r2['news_newsDetails_id'] == '' || $r2['news_newsDetails_id'] != $r['newsDetails_id']){
-			$detailnews->value = 'หากต้องการเพิ่มรายละเอียดกรุณาคลิก';
-			$detailnews2->value = 'หากต้องการเพิ่มรายละเอียดกรุณาคลิก';
-
-		}
-		else{
-				$detailnews->value = $r['newsDetails_name'];
-		}
+	$id=$_GET['id'];
+	$r = $db->findByPK('newsDetails','newsDetails_id',$id)->executeRow();
+	$detailnews->value = $r['newsDetails_name'];
 	}
-	echo $form->open("form_detail","form","col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12","","");
 ?>
 <div class='col-xl-1 col-lg-1 col-md-1 col-sm-1 col-1'></div>
 <div class='col-xl-10 col-lg-10 col-md-10 col-sm-10 col-10 pb-3' style="background-color:#ffffff;">
 	<div class='row'>
 		<div class='col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 mt-3'>
-			<?php
-				echo $lbheadnews;
-				echo $txtheadnews;
-			?>
+			<input class="form-control" type="text" value="Head NEWS"/>
 		</div>
 		<div class='col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 mt-3'>
 			<div class='row'>
@@ -41,9 +23,7 @@
 				<div class='col-xl-8 col-lg-8 col-md-8 col-sm-8 col-8'>
 					<div class='row'>
 						<div class='col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12' style="color:#007bff;">
-							<?php $rs = $db->findByPK22('news','user','user_user_id','user_id','news_id',$_GET['id'])->executeAssoc();
-								echo 'เพิ่มข่าวสารโดยคุณ ',$rs['user_name'],' ',$rs['user_last'],' ',$rs['news_dateupdate'];
-							?>
+							post by choatchaw 22 aug 2561 10.00am
 						</div>
 						<div class='col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 mt-3'>
 							<div class='row'>
@@ -51,13 +31,13 @@
 								<div class='col-xl-8 col-lg-8 col-md-8 col-sm-8 col-8'>
 									<?php
 										echo $lbvdo;
-										echo $filevideo;
+										echo $filepic;
 									?>
 								</div>
 								<div class='col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2'></div>
 							</div>
 						</div>
-						<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 mt-2 text_detail">
+						<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 mt-2">
 							<?php
 								echo $lbdetailnews;
 								echo $detailnews;
@@ -75,131 +55,48 @@
 								<div class='col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2'></div>
 							</div>
 						</div>
-						<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 mt-2 text_detail">
+						<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 mt-2">
 							<?php
 								echo $lbdetailnews;
-								echo $detailnews2;
+								echo $detailnews;
 							?>
-							<input type="submit" id="button_adddetail" value="บันทึก">
-							<input type="button" id="button_canceletail" value="ยกเลิก">
 						</div>
-						<div class='col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12'>
-					<div class='row'>
-						<div class='col-xl-3 col-lg-3 col-md-3 col-sm-3 col-3'></div>
-						<div class='col-xl-3 col-lg-3 col-md-3 col-sm-3 col-3'>
-								<span id="mySpan">
-								</span>
-					</div>
-						<div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col-3" id="add">
-							<input class="btn btn-primary col-12" name="btnButton" id="btnButton" type="button" value="เพิ่ม" onClick="JavaScript:fncCreateElement();">
-						</div>
-
-					</div>
 					</div>
 				</div>
 				<div class='col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2'></div>
 			</div>
 		</div>
+		<!--<div class='col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 mt-3 mb-3'>
+			<div class='row'>
+				<div class='col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2'>
+					<img height="100" width="100%" src='images/view.jpg' />
+				</div>
+				<div class='col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2'>
+					<img height="100" width="100%" src='images/view2.jpg' />
+				</div>
+				<div class='col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2'>
+					<img height="100" width="100%" src='images/view.jpg' />
+				</div>
+				<div class='col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2'>
+					<img height="100" width="100%" src='images/view2.jpg' />
+				</div>
+				<div class='col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2'>
+					<img height="100" width="100%" src='images/view.jpg' />
+				</div>
+				<div class='col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2'>
+					<img height="100" width="100%" src='images/view2.jpg' />
+				</div>
+			</div>
+		</div>-->
 		<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 mt-3">
 			<div class='row'>
 				<div class='col-xl-10 col-lg-10 col-md-10 col-sm-10 col-10'></div>
 				<div class='col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2'>
 					<input class="btn btn-success w-100" type="submit" value="submit" />
-					<input  type="hidden" id="new_id" name ="new_id" value="<?php echo $id;?>" />
-					<input  type="hidden" id="datetime" name="date_time" value="<?php echo $datetime;?>" />
-					<input  type="hidden" id="datetime" name="form_design" value="4" />
+					<input  type="hidden" id="id" value="<?php echo $id;?>" />
 				</div>
 			</div>
 		</div>
 	</div>
 </div>
 <div class='col-xl-1 col-lg-1 col-md-1 col-sm-1 col-1'></div>
-<?php
-    echo $form->close();
-?>
-<script>
-                $('#button_adddetail').hide();
-                $('#button_canceletail').hide();
-                $('.text_detail').on('click',function(){
-	                if(!CKEDITOR.instances['text_editer'] && !CKEDITOR.instances['text_editer2']){
-	                CKEDITOR.replace( 'text_editer', {
-					    uiColor: '#9AB8F3',
-					    removeButtons : 'Styles,Scayt,Format,Source,Strike,Maximize,About,Image',
-					    enterMode : CKEDITOR.ENTER_BR
-					});
-					CKEDITOR.replace( 'text_editer2', {
-					    uiColor: '#9AB8F3',
-					    removeButtons : 'Styles,Scayt,Format,Source,Strike,Maximize,About,Image',
-					    enterMode : CKEDITOR.ENTER_BR
-					});
-					}
-					$('#button_adddetail').show();
-					$('#button_canceletail').show();
-					$("#text_editer").removeAttr("readonly");
-					$("#text_editer").val('');
-					$("#text_editer2").removeAttr("readonly");
-					$("#text_editer2").val('');
-					$("form").on('submit',function(e) {
-						e.preventDefault();
-						CKEDITOR.instances[ 'text_editer' ].updateElement();
-						CKEDITOR.instances[ 'text_editer2' ].updateElement();
-						$.ajax({
-							url: "news_insert_detail.php",
-							type: "POST",
-							data:  new FormData(this),
-							contentType: false,
-							cache: false,
-							processData:false,
-							success: function(data) {
-								$("input[name*='detail_news[]']").val(data);
-								if(CKEDITOR.instances['text_editer']){
-									CKEDITOR.instances['text_editer'].destroy(true);
-									$('#text_editer').attr('readonly', true);
-									$('#button_adddetail').hide();
-									$('#button_canceletail').hide();
-								}
-
-							}
-						});
-						e.stopImmediatePropagation();
-					});
-					$('#button_canceletail').on('click',function(e){
-						if(CKEDITOR.instances['text_editer']){
-							$.ajax({
-					    	url: "news_insert_detail.php",
-					        data: {text : 'cancel'},
-					        type: "POST",
-					        success: function(data) {
-						        console.log(data);
-						        $('#text_editer').val(data);
-						        $('#text_editer2').val(data);
-						        if(CKEDITOR.instances['text_editer']){
-							    		CKEDITOR.instances['text_editer'].destroy(true);
-										$('#text_editer').attr('readonly', true);
-										$('#button_adddetail').hide();
-										$('#button_canceletail').hide();
-										e.stopPropagation();
-								}
-					        	}
-					    	});
-						}
-					});
-                });
-                   var i = 1 ;
-		$('#btnButton').on('click',function(){
-			i++;
-			console.log(i);
-			if(i == 6){
-				document.getElementById("add").innerHTML = "เพิ่มรูปภาพได้ไม่เกิน 6 รูปภาพครับ";
-				$('#add').addClass('text-danger');
-			}
-		});
-           function fncCreateElement(){
-
-		   		var mySpan = document.getElementById('mySpan');
-				var myElement1 = document.createElement('input');
-				myElement1.setAttribute('type',"file");
-				myElement1.setAttribute('name',"news_picdetail[]");
-				mySpan.appendChild(myElement1);
-	}
-</script>
