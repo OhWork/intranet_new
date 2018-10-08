da2699080bae94c84adb2bfa57e0e0d246966d04
Firore <takbasic722@gmail.com>
2018-10-08 22:38:06 +0700

    อื่น ๆ

--- branner.php
+++ branner.php
@@ -13,7 +13,7 @@
 			<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 bcmn" style="height:150px;padding-top:10px;border-radius:5px;"></div>
 		</a>
 		<a class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col-3">
-			<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 bcmn" style="height:150px;padding-top:10px;border-radius:5px;text-align:center;">อื่น ๆ</div>
+			<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 bcmn" style="height:150px;padding-top:60px;border-radius:5px;text-align:center;"><h4>อื่น ๆ</h4></div>
 		</a>
 	</div>
 </div>
