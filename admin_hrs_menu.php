81ccdb55817ff639a087b670a9a9995b4be4d5ce
Firore <takbasic722@gmail.com>
2018-10-09 21:34:43 +0700

    แก้ได้ละ

--- admin_hrs_menu.php
+++ admin_hrs_menu.php
@@ -1,7 +1,7 @@
 <a class="nav-link collapsed nav-link-19 edittext mucl" href="#hrs" data-toggle="collapse" data-target="#hrs"><span data-feather="calendar"></span>ระบบขอหนังสือรับรอง</a>
     <!-- sub menu -->
-    <ul class="sub-menu collapse mmpd" id="hrs">
-	    <a class="nav-link collapsed nav-link-20 edittext mucl" id="nav-19-animate-1" data-toggle="collapse" data-target="#hrscase">รายการหนังสือรับรอง<span data-feather="chevron-right"></span></a>
+    <ul class="sub-menu collapse on-sub mmpd" id="hrs">
+	    <a class="nav-link collapsedv nav-link-20 edittext mucl" id="nav-19-animate-1" href="#hrscase" data-toggle="collapse" data-target="#hrscase">รายการหนังสือรับรอง<span data-feather="chevron-right"></span></a>
 	        <ul class="sub-menu collapse mmpd2" id="hrscase">
 				<a class="nav-link edittext mucl" id="nav-20-animate-1" href="admin_index.php?url=hrs_show_editcertificate.php">รายการรอการดำเนินการ</a>
 				<a class="nav-link edittext mucl" id="nav-20-animate-2" href="admin_index.php?url=hrs_show_editcertificate_wait.php">รายการรอการอนุมัติ</a>
