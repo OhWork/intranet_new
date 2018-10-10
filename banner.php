c2a95e33199622a3c68d1063e63e99b817bee5a7
choatchaw <choatchaw@gmail.com>
2018-10-10 10:05:13 +0700

    ฟาย

--- banner.php
+++ banner.php
@@ -4,7 +4,7 @@
 <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 mt-3 pb-3">	
 	<div class="row">
 		<a class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col-3">
-			<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 bcmn" style="height:150px;padding-top:10px;border-radius:5px;"></div>
+			<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 bcmn" style="height:150px;padding-top:60px;border-radius:5px;text-align:center;"><h4>ระบบสารบรรณอิเล็กทรอนิกส์ สรอ.</h4></div>
 		</a>
 		<a class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col-3">
 			<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 bcmn" style="height:150px;padding-top:10px;border-radius:5px;"></div>
