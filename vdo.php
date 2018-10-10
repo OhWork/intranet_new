5307379d3ec5b4fc43f09c2d6fd8520a4edc1042
choatchaw <choatchaw@gmail.com>
2018-10-10 11:53:23 +0700

    เพิ่ม

--- vdo.php
+++ vdo.php
@@ -1,3 +1,3 @@
 <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">	
-<iframe class="w-100" height="526" src="https://www.youtube.com/embed/iQLiprbMmFg?autoplay=1&controls=0" allow='autoplay'></iframe>		
+<iframe class="w-100" height="526" src="https://www.youtube.com/embed/X33kx7RDeHs?autoplay=1&controls=0" allow='autoplay'></iframe>		
 </div>
