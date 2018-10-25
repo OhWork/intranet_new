ecf336c61a0eace7847b3bdcbbf876e4127e0798
Puttikun Wongpanyanurak <puttikun@hotmail.com>
2018-10-25 16:39:36 +0700

    ลองดูใหม่

--- news_insert_medie.php
+++ news_insert_medie.php
@@ -11,43 +11,76 @@
 	$rs5 = $db-> findByPK12('newsImg','newsImg_position',5,'newsImg_connect',$_POST['new_id'])->executeAssoc();
 	$rs6 = $db-> findByPK12('newsImg','newsImg_position',6,'newsImg_connect',$_POST['new_id'])->executeAssoc();
 	$rsvdo = $db-> findByPK12('newsVideo','newsVideo_position',1,'newsVideo_connect',$_POST['new_id'])->executeAssoc();
+	print_r($rs4);
 	if(!empty($rs['newsImg_id'])){
 		if(!empty($_FILES['news_picdetail'])){
+			$target_dir = 'temp/';
+			$target_file = $target_dir.basename($_FILES['news_picdetail']['name']);
+			$path = 'images/news/newsImg/';
+			$target_dir_save = $path.basename($_FILES['news_picdetail']['name']);
+			move_uploaded_file($_FILES['news_picdetail']['tmp_name'], $target_dir_save);
 			$data['newsImg_name'] = basename($_FILES['news_picdetail']['name']);
 			$rseditpic = $db->update2con('newsImg',$data,'newsImg_position',1,'newsImg_connect',$_POST['new_id']);
 		}
 	}
 	if(!empty($rs2['newsImg_id'])){
+		$target_dir = 'temp/';
+		$target_file = $target_dir.basename($_FILES['news_picdetail2']['name']);
+		$path = 'images/news/newsImg/';
+		$target_dir_save = $path.basename($_FILES['news_picdetail2']['name']);
+		move_uploaded_file($_FILES['news_picdetail2']['tmp_name'], $target_dir_save);
+
 		if(!empty($_FILES['news_picdetail2'])){
 			$data['newsImg_name'] = basename($_FILES['news_picdetail2']['name']);
 			$rseditpic = $db->update2con('newsImg',$data,'newsImg_position',2,'newsImg_connect',$_POST['new_id']);
 		}
 	}
 	if(!empty($rs3['newsImg_id'])){
+		$target_dir = 'temp/';
+		$target_file = $target_dir.basename($_FILES['news_picdetail3']['name']);
+		$path = 'images/news/newsImg/';
+		$target_dir_save = $path.basename($_FILES['news_picdetail3']['name']);
+		move_uploaded_file($_FILES['news_picdetail3']['tmp_name'], $target_dir_save);
 		if(!empty($_FILES['news_picdetail3'])){
 			$data['newsImg_name'] = basename($_FILES['news_picdetail3']['name']);
 			$rseditpic = $db->update2con('newsImg',$data,'newsImg_position',3,'newsImg_connect',$_POST['new_id']);
 		}
 	}
 	if(!empty($rs4['newsImg_id'])){
+		$target_dir = 'temp/';
+				$target_file = $target_dir.basename($_FILES['news_picdetail4']['name']);
+				$path = 'images/news/newsImg/';
+				$target_dir_save = $path.basename($_FILES['news_picdetail4']['name']);
+				move_uploaded_file($_FILES['news_picdetail4']['tmp_name'], $target_dir_save);
 		if(!empty($_FILES['news_picdetail4'])){
 			$data['newsImg_name'] = basename($_FILES['news_picdetail4']['name']);
 			$rseditpic = $db->update2con('newsImg',$data,'newsImg_position',4,'newsImg_connect',$_POST['new_id']);
 		}
 	}
 	if(!empty($rs5['newsImg_id'])){
+		$target_dir = 'temp/';
+				$target_file = $target_dir.basename($_FILES['news_picdetail5']['name']);
+				$path = 'images/news/newsImg/';
+				$target_dir_save = $path.basename($_FILES['news_picdetail5']['name']);
+				move_uploaded_file($_FILES['news_picdetail5']['tmp_name'], $target_dir_save);
 		if(!empty($_FILES['news_picdetail5'])){
 			$data['newsImg_name'] = basename($_FILES['news_picdetail5']['name']);
 			$rseditpic = $db->update2con('newsImg',$data,'newsImg_position',5,'newsImg_connect',$_POST['new_id']);
 		}
 	}
 	if(!empty($rs6['newsImg_id'])){
+		$target_dir = 'temp/';
+				$target_file = $target_dir.basename($_FILES['news_picdetail6']['name']);
+				$path = 'images/news/newsImg/';
+				$target_dir_save = $path.basename($_FILES['news_picdetail6']['name']);
+				move_uploaded_file($_FILES['news_picdetail6']['tmp_name'], $target_dir_save);
+
 		if(!empty($_FILES['news_picdetail6'])){
 			$data['newsImg_name'] = basename($_FILES['news_picdetail6']['name']);
 			$rseditpic = $db->update2con('newsImg',$data,'newsImg_position',6,'newsImg_connect',$_POST['new_id']);
 		}
 	}
-	if(!empty($rs6['newsImg_id'])){
+	if(!empty($rsvdo['newsVideo_id'])){
 		if($_POST['news_vdo'] != ''){
 			$data['newsVideo_link'] = $_POST['news_vdo'];
 			$rseditpic = $db->update2con('newsVideo',$data,'newsVideo_position',1,'newsVideo_connect',$_POST['new_id']);
