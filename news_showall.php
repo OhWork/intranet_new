ca7b3f9cd56e14337cbf715026d2cb9ef8f6c71e
Puttikun Wongpanyanurak <puttikun@hotmail.com>
2019-01-10 20:33:15 +0700

    อัพ

--- news_showall.php
+++ news_showall.php
@@ -23,61 +23,68 @@
 								$design = "news_designtype5";
 							}
 						?>
-							<div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-4 mt-2">
+							<div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col-3 ml-5 mt-2 bordernew">
 								<div class='row'>
-									<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 shadow" style="background-color:#ffffff;">
+									<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" style="background-color:#ffffff;">
 										<div class='row'>
-											<div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-4">
+											<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
 												<div class='row'>
-													<?php
-														if(!empty($show['news_cover'])){
-													?>
-													<img height="100" src='images/news/<?php echo $show['news_cover']; ?>' />
-														<?php
-															}else{
-														if($show['subzoo_zoo_zoo_id'] == 11){
-														?>
-                                                        	<img height="100" src='images/logo/Dusitzoo.png' />
-                                                        <?php
-	                                                    }else if($show['subzoo_zoo_zoo_id'] == 12){
-		                                                ?>
-		                                                   	<img height="100" src='images/logo/KKOzoo.png' />
-		                                                <?php
-	                                                     }else if($show['subzoo_zoo_zoo_id'] == 13){
-		                                                ?>
-		                                                   	<img height="100" src='images/logo/chiangmai.png' />
-		                                                <?php
-	                                                     }else if($show['subzoo_zoo_zoo_id'] == 14){
-		                                                ?>
-		                                                   	<img height="100" src='images/logo/Nakhonrachsimazoo.png' />
-		                                                <?php
-	                                                     }else if($show['subzoo_zoo_zoo_id'] == 15){
-		                                                ?>
-		                                                   	<img height="100" src='images/logo/Songkhlazoo.png' />
-		                                                <?php
-	                                                     }else if($show['subzoo_zoo_zoo_id'] == 16){
-		                                                ?>
-		                                                   	<img height="100" src='images/logo/Ubonzoo.png' />
-		                                                <?php
-	                                                     }else if($show['subzoo_zoo_zoo_id'] == 17){
-		                                                ?>
-		                                                   	<img height="100" src='images/logo/KKzoo.png' />
-		                                                <?php
-	                                                     }else{
-                                                        ?>
-                                                        	<img height="100" src='images/logo/ZPO.png' />
-                                                        <?php
-	                                                    }
-	                                                    }
-                                                        ?>
+													<div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col-3"></div>
+													<div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-6">
+														<div class="row">
+															<?php
+																if(!empty($show['news_cover'])){
+															?>
+																	<img height="100" src='images/news/<?php echo $show['news_cover']; ?>' />
+																<?php
+																	}else{
+																if($show['subzoo_zoo_zoo_id'] == 11){
+																?>
+		                                                        	<img height="100" src='images/logo/Dusitzoo.png' />
+		                                                        <?php
+			                                                    }else if($show['subzoo_zoo_zoo_id'] == 12){
+				                                                ?>
+				                                                   	<img height="100" src='images/logo/KKOzoo.png' />
+				                                                <?php
+			                                                     }else if($show['subzoo_zoo_zoo_id'] == 13){
+				                                                ?>
+				                                                   	<img height="100" src='images/logo/chiangmai.png' />
+				                                                <?php
+			                                                     }else if($show['subzoo_zoo_zoo_id'] == 14){
+				                                                ?>
+				                                                   	<img height="100" src='images/logo/Nakhonrachsimazoo.png' />
+				                                                <?php
+			                                                     }else if($show['subzoo_zoo_zoo_id'] == 15){
+				                                                ?>
+				                                                   	<img height="100" src='images/logo/Songkhlazoo.png' />
+				                                                <?php
+			                                                     }else if($show['subzoo_zoo_zoo_id'] == 16){
+				                                                ?>
+				                                                   	<img height="100" src='images/logo/Ubonzoo.png' />
+				                                                <?php
+			                                                     }else if($show['subzoo_zoo_zoo_id'] == 17){
+				                                                ?>
+				                                                   	<img height="100" src='images/logo/KKzoo.png' />
+				                                                <?php
+			                                                     }else{
+		                                                        ?>
+		                                                        	<img height="100" src='images/logo/ZPO.png' />
+		                                                        <?php
+			                                                    }
+			                                                    }
+		                                                        ?>
+														</div>
+													</div>
+													<div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col-3"></div>
 												</div>
 											</div>
-											<div class="col-xl-8 col-lg-8 col-md-8 col-sm-8 col-8">
+											<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
 												<div class='row'>
-													<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 mt-3">
+													<div class="col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2"></div>
+													<div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-4 mt-3">
 														<a href="index.php?url=<?php echo $design;?>.php&id=<?php echo $show['news_id']?>" style="color:#000000;"><?php echo $show['news_head']; ?></a>
 													</div>
-													<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" style="color:#8d8d8d;">
+													<div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-4 mt-3" style="color:#8d8d8d;">
 														<span data-feather="user"></span><?php echo ' ',$show['user_name'],' ',$show['user_last'],' ',$show['news_date']; ?>
 													</div>
 												</div>
