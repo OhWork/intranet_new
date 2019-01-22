9f830b8cbd4081efe30700e245787498015769c3
Puttikun Wongpanyanurak <puttikun@hotmail.com>
2019-01-22 21:25:13 +0700

    ตั้งแต่เย็น

--- cs_register.php
+++ cs_register.php
@@ -10,15 +10,16 @@
     $lbtel = new label('เบอร์ติดต่อ');
     $button = new buttonok("ส่งแบบฟอร์ม","","btn btn-success btn-lg btn-block bt3success col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12","");
     ?>
-        <script language = "JavaScript">
+	<script language = "JavaScript">
 
 		//**** List subzoo (Start) ***//
 		function ListSubzoo(SelectValue)
-		{
+{
 			frmMain.ddlSubzoo.length = 0
+
 			//*** Insert null Default Value ***//
-			var myOption = new Option('โปรดระบุ','null')
-			frmMain.ddlSubzoo.options[frmMain.ddlSubzoo.length]= myOption
+			var myOption = new Option('โปรดระบุ','');
+			frmMain.ddlSubzoo.options[frmMain.ddlSubzoo.length]= myOption;
 
 			<?php
 			$intRows = 0;
@@ -28,27 +29,72 @@
 			{
 			$intRows++;
 			?>
-				x = <?=$intRows;?>;
+				x = <?php echo $intRows;?>;
 				mySubList = new Array();
 
-				strGroup = <?=$objResult["zoo_zoo_id"];?>;
-				strGroup2 = <?=$objResult["subzoo_enable"];?>;
-				strValue = "<?=$objResult["subzoo_id"];?>";
-				strItem = "<?=$objResult["subzoo_name"];?>";
+				strGroup = <?php echo $objResult["zoo_zoo_id"];?>;
+				strValue = "<?php echo $objResult["subzoo_id"];?>";
+				strItem = "<?php echo $objResult["subzoo_name"];?>";
 				mySubList[x,0] = strItem;
 				mySubList[x,1] = strGroup;
-				mySubList[x,2] = strGroup2;
-				mySubList[x,3] = strValue;
-				if ((mySubList[x,1] == SelectValue) && (mySubList[x,2] == 1)){
-					var myOption = new Option(mySubList[x,0], mySubList[x,3])
+				mySubList[x,2] = strValue;
+				if (mySubList[x,1] == SelectValue){
+					var myOption = new Option(mySubList[x,0], mySubList[x,2])
 					frmMain.ddlSubzoo.options[frmMain.ddlSubzoo.length]= myOption
 				}
 			<?php
 			}
 			?>
 		}
+		function setDefault()
+		{
+			<?php
+				/*** ค่า Default ที่ได้จากการจัดเก็บ ***/
+				$r2 = $db->findByPK('user','user_id',$id)->executeRow();
+				$strZoo = $r2['subzoo_zoo_zoo_id'];
+				$strSubzoo = $r2['subzoo_subzoo_id'];
+			?>
+
+				<?php
+				/*** Default Zoo  ***/
+				if($strZoo != "")
+				{
+				?>
+					var objZoo=document.frmMain.ddlZoo;
+					for (x=0;x<objZoo.length;x++)
+					{
+						if (objZoo.options[x].value=="<?=$strZoo?>")
+						{
+							objZoo.options[x].selected = true;
+							break;
+						}
+					}
+
+					ListSubzoo(<?=$strZoo;?>)
+				<?php
+				}
+				?>
+
+				<?php
+				/*** Default Subzoo  ***/
+				if($strSubzoo != "")
+				{
+				?>
+					var objSubZoo=document.frmMain.ddlSubzoo;
+					for (x=0;x<objSubZoo.length;x++)
+					{
+						if (objSubZoo.options[x].value=="<?=$strSubzoo?>")
+						{
+							objSubZoo.options[x].selected = true;
+							break;
+						}
+					}
+				<?php
+				}
+				?>
+}
 </script>
-<?php echo $form->open("","","col-xl-10 col-lg-10 col-md-10 col-sm-10 col-10","hrs_insert_hospital.php",""); ?>
+<?php echo $form->open("","frmMain","col-xl-10 col-lg-10 col-md-10 col-sm-10 col-10","hrs_insert_hospital.php",""); ?>
 <div class="row">
 	<div class="col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2"></div>
 	<div class="col-xl-8 col-lg-8 col-md-8 col-sm-8 col-8" style="padding-top:16px;background-color:#ffffff;border:solid 1px #E0E0E0;border-radius:7px;">
@@ -75,24 +121,25 @@
 				<?php echo $lbdevision; ?>
 			</div>
 			<div class='col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 form-group has-feedback'>
-                                                                <select class='form-control' id="ddlZoo" name="subzoo_zoo_zoo_id" onchange = "ListSubzoo(this.value)">
-					<option selected value="">โปรดระบุ</option>
-						<?php $rs = $db->findAllASC('zoo','zoo_no')->execute();
-						while($objResult = mysqli_fetch_array($rs,MYSQLI_ASSOC))
-						{
-						?>
-						<option value="<?=$objResult["zoo_id"];?>"><?=$objResult["zoo_name"];?></option>
-						<?php
-						}
-						?>
-				</select>
-			</div>
-                                                <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-				<label>ฝ่าย</label>
-			</div>
-			<div class='col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 form-group has-feedback'>
-                                                                <select class='form-control' id="ddlSubzoo" name="subzoo_subzoo_id"></select>
-			</div>
+							<select class='form-control css-require' id="ddlZoo" name="subzoo_zoo_zoo_id" onChange = "ListSubzoo(this.value)">
+							<option selected value="">-----โปรดระบุ-----</option>
+							<?php
+								$rs = $db->orderASC('zoo','zoo_id')->execute();
+								while($objResult = mysqli_fetch_array($rs,MYSQLI_ASSOC))
+								{
+							?>
+								<option value="<?=$objResult["zoo_id"];?>"><?=$objResult["zoo_name"];?></option>
+							<?php
+								}
+								?>
+							</select>
+				</div>
+				<div class='col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12'>
+					<label>ฝ่าย</label>
+				</div>
+				<div class='col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 form-group has-feedback'>
+					<select class='form-control css-require' id="ddlSubzoo" name="subzoo_subzoo_id"></select>
+				</div>
 			<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
 				<?php echo $lbwork; ?>
 			</div>
