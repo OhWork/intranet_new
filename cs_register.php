d8ee7ce28dcccc27baf949194f8f6c692c8a6ee4
choatchaw <Choatchaw@live.com>
2019-01-23 09:55:45 +0700

    นี่หละเหงาา

--- cs_register.php
+++ cs_register.php
@@ -172,46 +172,46 @@
 					<div class="col-xl-8 col-lg-8 col-md-8 col-sm-8 col-8">
 						<div class="row">
 							<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-								<input type="checkbox" name="hrhos_familytype" id="parent1" value="1"> ระบบอินเทอร์เน็ตองค์การสวนสัตว์
+								<input type="checkbox" name="reguser_internet_use" id="parent1" value="1"> ระบบอินเทอร์เน็ตองค์การสวนสัตว์
 							</div>
 							<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-								<input type="checkbox" name="hrhos_familytype"  id="parent2" value="2"> ระบบอินเทอร์เน็ตโมบาย
+								<input type="checkbox" name="reguser_minternet_use"  id="parent2" value="2"> ระบบอินเทอร์เน็ตโมบาย
 							</div>
 							<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-								<input type="checkbox" name="hrhos_familytype"  id="parent3" value="3"> ระบบ ZPO data center
+								<input type="checkbox" name="reguser_intranet_use"  id="parent3" value="3"> ระบบ ZPO data center
 							</div>
 							<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-									<input type="checkbox" name="hrhos_familytype" id="parent4" value="4"> ระบบ E-Project
+									<input type="checkbox" name="reguser_eproject_use" id="parent4" value="4"> ระบบ E-Project
 							</div>
 							<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-								<input type="checkbox" name="hrhos_familytype" id="parent5" value="5"> ระบบทะเบียนสัตว์
+								<input type="checkbox" name="reguser_animal_use" id="parent5" value="5"> ระบบทะเบียนสัตว์
 							</div>
 							<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-								<input type="checkbox" name="hrhos_familytype" id="parent5" value="5"> ระบบบริหารงานบุคคล
+								<input type="checkbox" name="reguser_hrsys_use" id="parent5" value="5"> ระบบบริหารงานบุคคล
 							</div>
 							<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-								<input type="checkbox" name="hrhos_familytype" id="parent5" value="5"> www.zoothailand.org
+								<input type="checkbox" name="reguser_website_use" id="parent5" value="5"> www.zoothailand.org
 							</div>
 							<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-								<input type="checkbox" name="hrhos_familytype" id="parent5" value="5"> ระบบสารบรรณอิเล็กทรอนิกส์ สรอ.
+								<input type="checkbox" name="reguser_esarabun_use" id="parent5" value="5"> ระบบสารบรรณอิเล็กทรอนิกส์ สรอ.
 							</div>
 							<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
 								<div class="row">
 									<div class="col-xl-8 col-lg-8 col-md-8 col-sm-8 col-8">
-										<input type="checkbox" name="hrhos_familytype" id="forgetusercheck" value="5"> Username และ Password ในกรณีทำหายของระบบ
+										<input type="checkbox" name="reguser_userpasslost" id="forgetusercheck" value="5"> Username และ Password ในกรณีทำหายของระบบ
 									</div>
 									<div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-4">
-										<input type="text" name="name" id="forgetuser" class="form-control">
+										<input type="text" name="reguser_sent_email" id="forgetuser" class="form-control">
 									</div>
 								</div>
 							</div>
 							<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
 								<div class="row">
 									<div class="col-xl-8 col-lg-8 col-md-8 col-sm-8 col-8">
-										<input type="checkbox" name="hrhos_familytype" id="ohter" value="5"> อื่นๆ
+										<input type="checkbox" name="reguser_other" id="ohter" value="5"> อื่นๆ
 									</div>
 									<div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-4">
-										<input type="text" name="name" id="textohter" class="form-control">
+										<input type="text" name="reguser_other_detail" id="textohter" class="form-control">
 									</div>
 								</div>
 							</div>
