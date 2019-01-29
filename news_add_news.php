bf02c0a31a137720ed61dc698fd46871afa4131f
Puttikun Wongpanyanurak <puttikun@hotmail.com>
2019-01-29 08:55:00 +0700

    อะอัพไป

--- news_add_news.php
+++ news_add_news.php
@@ -237,8 +237,10 @@
 	    $('#preimg').show();
         readURL(this);
     });
+/*
 	var id_new = $('#new_id').val();
 	if(id_new != ''){
 	$('input[name=typeDesignnews_id]').slice(0).prop("disabled", true);
 	}
+*/
 </script>
