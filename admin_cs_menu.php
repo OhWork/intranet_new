3f93abdd57ab82837aab86a36ffa79a244d74348
Puttikun Wongpanyanurak <puttikun@hotmail.com>
2019-02-11 23:41:42 +0700

    อะ

--- admin_cs_menu.php
+++ admin_cs_menu.php
@@ -57,7 +57,9 @@ if ($user_zoo == 10 || $user_zoo == 18) {
     <ul class="sub-menu collapse mmpd2" id="csipacc">
         <a class="nav-link edittext mucl" id="nav-10-animate-1" href="admin_index.php?url=cs_show_iptools.php&id=1">Server</a>
     </ul>
-    <a class="nav-link collapsed py-1 nav-link-11 edittext mucl" id="nav-1-animate-4" href="#csreport" data-toggle="collapse" data-target="#csreport">รายงาน<span data-feather="chevron-right"></a>
+       <a class="nav-link collapsed py-1 edittext mucl" id="nav-1-animate-4" href="admin_index.php?url=cs_show_register.php">รายการขอคำร้องขอใช้ระบบ</a>
+    <a class="nav-link collapsed py-1 edittext mucl" id="nav-1-animate-5" href="admin_index.php?url=cs_showupweb.php">รายการขอคำร้องขึ้นเว็บไซต์</a>
+    <a class="nav-link collapsed py-1 nav-link-11 edittext mucl" id="nav-1-animate-6" href="#csreport" data-toggle="collapse" data-target="#csreport">รายงาน<span data-feather="chevron-right"></a>
     <ul class="sub-menu collapse mmpd2" id="csreport">
         <a class="nav-link collapsed py-1 bnmenusub2 nav-link-12 edittext mucl" id="nav-11-animate-1" href="#csreportcom" data-toggle="collapse" data-target="#csreportcom">รายงานการซ่อม<span data-feather="chevron-right"></span></a>
         <ul class="sub-menu collapse mmpd2 " id="csreportcom">
@@ -153,13 +155,11 @@ if ($user_zoo == 10 || $user_zoo == 18) {
 <?php } ?>
         </ul>
     </ul>
-    <a class="nav-link collapsed py-1 edittext mucl" id="nav-1-animate-5" href="admin_index.php?url=cs_showupweb.php">รายการขอคำร้องขึ้นเว็บไซต์</a>
-    <a class="nav-link collapsed py-1 nav-link-14 edittext mucl" id="nav-1-animate-6" href="#cssetting" data-toggle="collapse" data-target="#cssetting"><span data-feather="settings"></span>ตั้งค่า<span data-feather="chevron-right"></span></a>
+    <a class="nav-link collapsed py-1 nav-link-14 edittext mucl" id="nav-1-animate-7" href="#cssetting" data-toggle="collapse" data-target="#cssetting"><span data-feather="settings"></span>ตั้งค่า<span data-feather="chevron-right"></span></a>
     <ul class="sub-menu collapse mmpd2" id="cssetting">
         <a class="nav-link edittext mucl" id="nav-14-animate-1" href="admin_index.php?url=cs_add_typetools.php">จัดการชนิดอุปกรณ์ (แจ้งซ่อม)</a>
         <a class="nav-link edittext mucl" id="nav-14-animate-2" href="admin_index.php?url=cs_add_problemlist.php">จัดการรายการปัญหา (แจ้งซ่อม)</a>
         <a class="nav-link edittext mucl" id="nav-14-animate-3" href="admin_index.php?url=cs_add_typeworkupweb.php">จัดการระบบ (ร้องขอขึ้นเว็บ)</a>
     </ul>
-    <a class="nav-link collapsed py-1 edittext mucl" id="nav-1-animate-7" href="admin_index.php?url=cs_showregister.php">รายการขอคำร้องขอใช้ระบบ</a>
 </ul>
 <!-- end sub menu -->
