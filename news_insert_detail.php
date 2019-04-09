3145534abc026bf3778e6933152ddbc264782197
Puttikun Wongpanyanurak <puttikun@hotmail.com>
2019-04-09 09:54:57 +0700

    ใหม่ๆแก้ไม่หมด

--- news_insert_detail.php
+++ news_insert_detail.php
@@ -14,20 +14,20 @@
 
 // 		print_r($_POST);
 			$data['newsDetails_name'] = $_POST['detail_news'];
-			$rseditdetail = $db->update2con('newsDetails',$data,'newsDetails_position',1,'newsDetails_connect',$_POST['new_id']);
+			$rseditdetail = $db->update2con('newsdetails',$data,'newsDetails_position',1,'newsDetails_connect',$_POST['new_id']);
 			if(!empty($_POST['detail_news2'])){
 			$data2['newsDetails_name'] = $_POST['detail_news2'];
-			$rseditdetail = $db->update2con('newsDetails',$data2,'newsDetails_position',2,'newsDetails_connect',$_POST['new_id']);
+			$rseditdetail = $db->update2con('newsdetails',$data2,'newsDetails_position',2,'newsDetails_connect',$_POST['new_id']);
 			}
 		}else{
-				$rs = $db->insert('newsDetails',array(
+				$rs = $db->insert('newsdetails',array(
 					'newsDetails_name' => $_POST['detail_news'],
 					'newsDetails_position' => 1,
 					'newsDetails_connect' => $_POST['new_id']
 
 				));
 				if(!empty($_POST['detail_news2'])){
-					$rs2 = $db->insert('newsDetails',array(
+					$rs2 = $db->insert('newsdetails',array(
 					'newsDetails_name' => $_POST['detail_news2'],
 					'newsDetails_position' => 2,
 					'newsDetails_connect' => $_POST['new_id']
@@ -39,11 +39,11 @@
 		if(@$rs || @$rs2 || @$rseditdetail){
 			$data['news_dateupdate'] = $_POST['date_time'];
 			$rseditdate = $db->update('news',$data,'news_id',$_POST['new_id']);
-				$selectiddetail = $db->findAllDESC('newsDetails','newsDetails_id')->executeAssoc();
+				$selectiddetail = $db->findAllDESC('newsdetails','newsDetails_id')->executeAssoc();
 				$lastiddetail = $selectiddetail['newsDetails_id'];
-				$rsshowdetail = $db->findByPK12('newsDetails','newsDetails_position',1,'newsDetails_connect',$_POST['new_id'])->executeAssoc();
+				$rsshowdetail = $db->findByPK12('newsdetails','newsDetails_position',1,'newsDetails_connect',$_POST['new_id'])->executeAssoc();
 				if($form_design == 4){
-					$rsshowdetail2 = $db->findByPK('newsDetails','newsDetails_connect',$_POST['new_id'])->execute();
+					$rsshowdetail2 = $db->findByPK('newsdetails','newsDetails_connect',$_POST['new_id'])->execute();
 					foreach($rsshowdetail2 as $abc){
 						$json_data[]=array(
 						 'detail'=>$abc['newsDetails_name'],
@@ -65,7 +65,7 @@
 		$rsshowdetail = $db->findByPK('newsdetails','newsDetails_id',$_POST['last_detail_id'])->executeAssoc();
 		if(!empty($_POST['last_detail_id'])){
 			if($form_design == 4){
-				$rsshowdetail2 = $db->findByPK('newsDetails','newsDetails_connect',$_POST['new_id'])->execute();
+				$rsshowdetail2 = $db->findByPK('newsdetails','newsDetails_connect',$_POST['new_id'])->execute();
 				foreach($rsshowdetail2 as $abc){
 					$json_data[]=array(
 					 'detail'=>$abc['newsDetails_name'],
