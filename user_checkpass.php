dc5d60ae61bbe4dbd95ac898c3a457745b07fce0
Choatchaw <Choatchaw@gmail.com>
2019-04-30 22:08:58 +0700

    กูบอกแล้ว

--- user_checkpass.php
+++ user_checkpass.php
@@ -1,12 +1,12 @@
 <?php
 	ob_start();
 	session_start();
-	error_reporting(0);
-    include 'database/db_tools.php';
+//	error_reporting(0);
+        include 'database/db_tools.php';
 	include 'connect.php';
 	$iduser = $_POST['userid'];
 	$passold = $_POST['passold'];
-        $strPassword = mysqli_real_escape_string($conn,md5(md5(md5($passold))));
+        $strPassword = md5(md5(md5($passold)));
 	$rs = $db->findbyPK('user','user_id',$iduser)->executeAssoc();
 	if($rs){
 		if($rs['user_pass'] != $strPassword){
