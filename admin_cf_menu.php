580394683e1e30f020d97431d62c7aa98a74c45f
Puttikun Wongpanyanurak <puttikun@hotmail.com>
2019-05-02 22:31:42 +0700

    อัพๆ

--- admin_cf_menu.php
+++ admin_cf_menu.php
@@ -6,6 +6,7 @@
               <ul class="sub-menu collapse on-sub mmpd" id="confer">
                     <a class="nav-link collapsed py-1 nav-link-3 edittext mucl" id="nav-2-animate-1" href="#cf" data-toggle="collapse" data-target="#cf">จัดการห้องประชุม</a>
 	                <ul class="sub-menu collapse bnmenusub1" id="cf">
+		           <?php if ($user_zoo == 10 || $user_zoo == 3) { ?>
 	                <a class="nav-link dropdown-item bnmenusub2 edittext mucl" id="nav-3-animate-1"href="#cfsub" data-toggle="collapse" data-target="#cfsub"><span data-feather="chevron-right"></span>องค์การสวนสัตว์</a>
     	                <ul class="sub-menu collapse bnmenusub1" id="cfsub">
     	                <?php $rsroomzpo =$db->countTable('conferroom','zoo_zoo_id',3)->execute();
@@ -25,6 +26,11 @@
     				             <li class="nav-link "><a class="dropdown-item" href="#">ยังไม่มีข้อมูล</a></li>
     			      <?php } ?>
     			      </ul>
+    			<?php
+        }
+        if ($user_zoo == 10 || $user_zoo == 11) {
+            ?>
+
     			      <a class="nav-link dropdown-item bnmenusub2 edittext mucl" id="nav-3-animate-2" href="#cfsub2" data-toggle="collapse" data-target="#cfsub2"><span data-feather="chevron-right"></span>สวนสัตว์ดุสิต</a>
     	                <ul class="sub-menu collapse bnmenusub1" id="cfsub2">
     	                <?php $rsroomzpo =$db->countTable('conferroom','zoo_zoo_id',11)->execute();
@@ -44,6 +50,10 @@
     				             <li class="nav-link dropdown-item"><a class="dropdown-item" href="#">ยังไม่มีข้อมูล</a></li>
     			      <?php } ?>
     			      </ul>
+    			       <?php
+        }
+        if ($user_zoo == 10 || $user_zoo == 12) {
+            ?>
     			      <a class="nav-link dropdown-item bnmenusub2 edittext mucl" id="nav-3-animate-3" href="#cfsub3" data-toggle="collapse" data-target="#cfsub3"><span data-feather="chevron-right"></span>สวนสัตว์เปิดเขาเขียว</a>
     	                <ul class="sub-menu collapse bnmenusub1" id="cfsub3">
     	                <?php $rsroomzpo =$db->countTable('conferroom','zoo_zoo_id',12)->execute();
@@ -63,6 +73,10 @@
     				             <li class="nav-link"><a class="dropdown-item" href="#">ยังไม่มีข้อมูล</a></li>
     			      <?php } ?>
     			      </ul>
+    			       <?php
+				        }
+				        if ($user_zoo == 10 || $user_zoo == 13) {
+				            ?>
     			      <a class="nav-link dropdown-item bnmenusub2 edittext mucl" id="nav-3-animate-4" href="#cfsub4" data-toggle="collapse" data-target="#cfsub4"><span data-feather="chevron-right"></span>สวนสัตว์เชียงใหม่</a>
     	                <ul class="sub-menu collapse bnmenusub1" id="cfsub4">
     	                <?php $rsroomzpo =$db->countTable('conferroom','zoo_zoo_id',13)->execute();
@@ -82,6 +96,11 @@
     				             <li class="nav-link"><a class="dropdown-item" href="#">ยังไม่มีข้อมูล</a></li>
     			      <?php } ?>
     			      </ul>
+    			      <?php
+				        }
+				        if ($user_zoo == 10 || $user_zoo == 14) {
+				            ?>
+
     			      <a class="nav-link dropdown-item bnmenusub2 edittext mucl" id="nav-3-animate-5" href="#cfsub5" data-toggle="collapse" data-target="#cfsub5"><span data-feather="chevron-right"></span>สวนสัตว์นครราชสีมา</a>
     	                <ul class="sub-menu collapse bnmenusub1" id="cfsub5">
     	                <?php $rsroomzpo =$db->countTable('conferroom','zoo_zoo_id',14)->execute();
@@ -101,6 +120,10 @@
     				             <li class="nav-link"><a class="dropdown-item" href="#">ยังไม่มีข้อมูล</a></li>
     			      <?php } ?>
     			      </ul>
+<?php
+        }
+        if ($user_zoo == 10 || $user_zoo == 15) {
+            ?>
     			      <a class="nav-link dropdown-item bnmenusub2 edittext mucl" id="nav-3-animate-6"  href="#cfsub6" data-toggle="collapse" data-target="#cfsub6"><span data-feather="chevron-right"></span>สวนสัตว์สงขลา</a>
     	                <ul class="sub-menu collapse bnmenusub1" id="cfsub6">
     	                <?php $rsroomzpo =$db->countTable('conferroom','zoo_zoo_id',15)->execute();
@@ -120,6 +143,11 @@
     				             <li class="nav-link "><a class="dropdown-item" href="#">ยังไม่มีข้อมูล</a></li>
     			      <?php } ?>
     			      </ul>
+					  	<?php
+        }
+        if ($user_zoo == 10 || $user_zoo == 16) {
+            ?>
+
     			      <a class="nav-link dropdown-item bnmenusub2 edittext mucl" id="nav-3-animate-7" href="#cfsub7" data-toggle="collapse" data-target="#cfsub7"><span data-feather="chevron-right"></span>สวนสัตว์อุบลราชธานี</a>
     	                <ul class="sub-menu collapse bnmenusub1" id="cfsub7">
     	                <?php $rsroomzpo =$db->countTable('conferroom','zoo_zoo_id',16)->execute();
@@ -139,6 +167,10 @@
     				             <li class="nav-link"><a class="dropdown-item" href="#">ยังไม่มีข้อมูล</a></li>
     			      <?php } ?>
     			      </ul>
+    			      <?php
+        }
+        if ($user_zoo == 10 || $user_zoo == 17) {
+            ?>
     			      <a class="nav-link dropdown-item bnmenusub2 edittext mucl" id="nav-3-animate-8" href="#cfsub8" data-toggle="collapse" data-target="#cfsub8"><span data-feather="chevron-right"></span>สวนสัตว์ขอนแก่น</a>
     	                <ul class="sub-menu collapse bnmenusub1" id="cfsub8">
     	                <?php $rsroomzpo =$db->countTable('conferroom','zoo_zoo_id',17)->execute();
@@ -158,6 +190,10 @@
     				             <li class="nav-link"><a class="dropdown-item" href="#">ยังไม่มีข้อมูล</a></li>
     			      <?php } ?>
     			      </ul>
+    			      <?php
+						}
+						if ($user_zoo == 10 || $user_zoo == 18) {
+						    ?>
     			      <a class="nav-link dropdown-item bnmenusub2 edittext mucl" id="nav-3-animate-9" href="#cfsub9" data-toggle="collapse" data-target="#cfsub9"><span data-feather="chevron-right"></span>คชอาณาจักร จ.สุรินทร์</a>
     	                <ul class="sub-menu collapse bnmenusub1" id="cfsub9">
     	                <?php $rsroomzpo =$db->countTable('conferroom','zoo_zoo_id',18)->execute();
@@ -177,11 +213,12 @@
     				             <li class="nav-link"><a class="dropdown-item" href="#">ยังไม่มีข้อมูล</a></li>
     			      <?php } ?>
     			      </ul>
+					  <?php }?>
 	                </ul>
     	            <a class="nav-link collapsed py-1 nav-link-4 edittext mucl" id="nav-2-animate-2" href="#cfo" data-toggle="collapse" data-target="#cfo">จัดการห้องประชุมทางไกล</a>
 
 	                <ul class="sub-menu collapse bnmenusub1" id="cfo">
-
+					 <?php if ($user_zoo == 10 || $user_zoo == 3) { ?>
 	                <a class="nav-link dropdown-item bnmenusub2 edittext mucl" id="nav-4-animate-1" href="#cfosub" data-toggle="collapse" data-target="#cfosub"><span data-feather="chevron-right"></span>องค์การสวนสัตว์</a>
     	                <ul class="sub-menu collapse bnmenusub1" id="cfosub">
     	                <?php $rsroomzpo =$db->countTable('conferroom','zoo_zoo_id',3)->execute();
@@ -201,6 +238,10 @@
     				             <li class="nav-link"><a class="dropdown-item" href="#">ยังไม่มีข้อมูล</a></li>
     			      <?php } ?>
     			      </ul>
+    			      <?php
+				        }
+				        if ($user_zoo == 10 || $user_zoo == 11) {
+				            ?>
     			      <a class="nav-link dropdown-item bnmenusub2 edittext mucl" id="nav-4-animate-2" href="#cfosub2" data-toggle="collapse" data-target="#cfosub2"><span data-feather="chevron-right"></span>สวนสัตว์ดุสิต</a>
     	                <ul class="sub-menu collapse bnmenusub1" id="cfosub2">
     	                <?php $rsroomzpo =$db->countTable('conferroom','zoo_zoo_id',11)->execute();
@@ -220,6 +261,10 @@
     				             <li class="nav-link"><a class="dropdown-item" href="#">ยังไม่มีข้อมูล</a></li>
     			      <?php } ?>
     			      </ul>
+    			       <?php
+        }
+        if ($user_zoo == 10 || $user_zoo == 12) {
+            ?>
     			      <a class="nav-link dropdown-item bnmenusub2 edittext mucl" id="nav-4-animate-3" href="#cfosub3" data-toggle="collapse" data-target="#cfosub3"><span data-feather="chevron-right"></span>สวนสัตว์เปิดเขาเขียว</a>
     	                <ul class="sub-menu collapse bnmenusub1" id="cfosub3">
     	                <?php $rsroomzpo =$db->countTable('conferroom','zoo_zoo_id',12)->execute();
@@ -239,6 +284,10 @@
     				             <li class="nav-link"><a class="dropdown-item" href="#">ยังไม่มีข้อมูล</a></li>
     			      <?php } ?>
     			      </ul>
+    			      <?php
+        }
+        if ($user_zoo == 10 || $user_zoo == 13) {
+            ?>
     			      <a class="nav-link dropdown-item bnmenusub2 edittext mucl" id="nav-4-animate-4" href="#cfosub4" data-toggle="collapse" data-target="#cfosub4"><span data-feather="chevron-right"></span>สวนสัตว์เชียงใหม่</a>
     	                <ul class="sub-menu collapse bnmenusub1" id="cfosub4">
     	                <?php $rsroomzpo =$db->countTable('conferroom','zoo_zoo_id',13)->execute();
@@ -258,6 +307,10 @@
     				             <li class="nav-link"><a class="dropdown-item" href="#">ยังไม่มีข้อมูล</a></li>
     			      <?php } ?>
     			      </ul>
+    			      <?php
+        }
+        if ($user_zoo == 10 || $user_zoo == 14) {
+            ?>
     			      <a class="nav-link dropdown-item bnmenusub2 edittext mucl" id="nav-4-animate-5" href="#cfosub5" data-toggle="collapse" data-target="#cfosub5"><span data-feather="chevron-right"></span>สวนสัตว์นครราชสีมา</a>
     	                <ul class="sub-menu collapse bnmenusub1" id="cfosub5">
     	                <?php $rsroomzpo =$db->countTable('conferroom','zoo_zoo_id',14)->execute();
@@ -277,6 +330,11 @@
     				             <li class="nav-link"><a class="dropdown-item" href="#">ยังไม่มีข้อมูล</a></li>
     			      <?php } ?>
     			      </ul>
+    			       <?php
+        }
+        if ($user_zoo == 10 || $user_zoo == 15) {
+            ?>
+
     			      <a class="nav-link dropdown-item bnmenusub2 edittext mucl" id="nav-4-animate-6" href="#cfosub6" data-toggle="collapse" data-target="#cfosub6"><span data-feather="chevron-right"></span>สวนสัตว์สงขลา</a>
     	                <ul class="sub-menu collapse bnmenusub1" id="cfosub6">
     	                <?php $rsroomzpo =$db->countTable('conferroom','zoo_zoo_id',15)->execute();
@@ -296,6 +354,10 @@
     				             <li class="nav-link"><a class="dropdown-item" href="#">ยังไม่มีข้อมูล</a></li>
     			      <?php } ?>
     			      </ul>
+    			      <?php
+        }
+        if ($user_zoo == 10 || $user_zoo == 16) {
+            ?>
     			      <a class="nav-link dropdown-item bnmenusub2 edittext mucl" id="nav-4-animate-7" href="#cfosub7" data-toggle="collapse" data-target="#cfosub7"><span data-feather="chevron-right"></span>สวนสัตว์อุบลราชธานี</a>
     	                <ul class="sub-menu collapse bnmenusub1" id="cfosub7">
     	                <?php $rsroomzpo =$db->countTable('conferroom','zoo_zoo_id',16)->execute();
@@ -315,6 +377,10 @@
     				             <li class="nav-link"><a class="dropdown-item" href="#">ยังไม่มีข้อมูล</a></li>
     			      <?php } ?>
     			      </ul>
+    			       <?php
+        }
+        if ($user_zoo == 10 || $user_zoo == 17) {
+            ?>
     			      <a class="nav-link dropdown-item bnmenusub2 edittext mucl" id="nav-4-animate-8" href="#cfosub8" data-toggle="collapse" data-target="#cfosub8"><span data-feather="chevron-right"></span>สวนสัตว์ขอนแก่น</a>
     	                <ul class="sub-menu collapse bnmenusub1" id="cfosub8">
     	                <?php $rsroomzpo =$db->countTable('conferroom','zoo_zoo_id',17)->execute();
@@ -334,6 +400,10 @@
     				             <li class="nav-link"><a class="dropdown-item" href="#">ยังไม่มีข้อมูล</a></li>
     			      <?php } ?>
     			      </ul>
+    			      <?php
+}
+if ($user_zoo == 10 || $user_zoo == 18) {
+    ?>
     			      <a class="nav-link dropdown-item bnmenusub2 edittext mucl" id="nav-4-animate-9" href="#cfosub9" data-toggle="collapse" data-target="#cfosub9"><span data-feather="chevron-right"></span>คชอาณาจักร จ.สุรินทร์</a>
     	                <ul class="sub-menu collapse bnmenusub1" id="cfosub9">
     	                <?php $rsroomzpo =$db->countTable('conferroom','zoo_zoo_id',18)->execute();
@@ -353,6 +423,7 @@
     				             <li class="nav-link"><a class="dropdown-item" href="#">ยังไม่มีข้อมูล</a></li>
     			      <?php } ?>
     			      </ul>
+    			      <?php }?>
 	                </ul>
 	                <a class="nav-link collapsed py-1 nav-link-5 edittext mucl" id="nav-2-animate-3" href="#cfreport" data-toggle="collapse" data-target="#cfreport">รายงาน</a>
 		                <ul class="sub-menu collapse bnmenusub1" id="cfreport">
