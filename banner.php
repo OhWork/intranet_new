359f60026b780b9198546f60e0cb1a8c345d8e3f
Puttikun Wongpanyanurak <puttikun@hotmail.com>
2019-05-09 22:09:49 +0700

    cyr

--- banner.php
+++ banner.php
@@ -1,19 +1,15 @@
 <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 pt-2 alltxh">
 	<h4>ลิงค์ระบบงาน และ เว็บไซต์ของหน่วยงานที่เกี่ยวข้อง</h4>
 </div>
-<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 mt-3 pb-3">	
+<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 mt-3 pb-3">
 	<div class="row">
-		<div class="col-xl-3 col-lg-3 col-md-3 col-sm-12 col-12" href="http://saraban.egov.go.th">
-			<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 bcmn bner"><h5>ระบบสารบรรณอิเล็กทรอนิกส์ สรอ.</h5></div>
+		<?php
+			$rs = $db->findbyPK('bn','bn_enable',1)->execute();
+			foreach($rs as $showrs){
+		?>
+		<div class="col-xl-3 col-lg-3 col-md-3 col-sm-12 col-12" href="<?php echo $showrs['bn_link'];?>">
+			<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 bcmn bner"><h5><?php echo $showrs['bn_name'];?></h5></div>
 		</div>
-		<div class="col-xl-3 col-lg-3 col-md-3 col-sm-12 col-12" href="https://accounts.mail.go.th">
-			<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 bcmn bner"><h5>ระบบ e-mail @zoothailand.mail.go.th (สรอ.)</h5></div>
-		</div>
-		<div class="col-xl-3 col-lg-3 col-md-3 col-sm-12 col-12" href="http://192.168.0.23">
-			<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 bcmn bner"><h5>ระบบทะเบียนสัตว์</h5></div>
-		</div>
-		<div class="col-xl-3 col-lg-3 col-md-3 col-sm-12 col-12" href="#">
-			<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 bcmn bner"><h5>อื่น ๆ</h5></div>
-		</div>
+		<?php }?>
 	</div>
 </div>
