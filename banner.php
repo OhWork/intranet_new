76272bfb750affe665b952536fe03538435356f9
Firore <takbasic722@gmail.com>
2019-05-10 22:54:26 +0700

    ลองอีกที

--- banner.php
+++ banner.php
@@ -13,13 +13,8 @@
 			</a>
 		</div>
 		<?php }?>
-		<div class="col-xl-3 col-lg-3 col-md-3 col-sm-12 col-12" >
-			<a href="<?php echo $showrs['bn_link'];?>">
-				<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 bcmn bner"><h5><?php echo $showrs['bn_name'];?></h5></div>
-			</a>
-		</div>
 	</div>
 </div>
-<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 mt-1 pb-3 view-bn">
+<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 mt-1 pb-3 view-bn" align="right">
 	<a href="index.php?url=banner_show_all.php" class="view-all pl-3 pr-3 pt-1 pb-1">ดูทั้งหมด</a>
 </div>
