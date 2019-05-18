af5cd143d0fc4ae20b5a7e31fb633be2e817dc73
Puttikun Wongpanyanurak <puttikun@hotmail.com>
2019-05-18 10:08:53 +0700

    อะ

--- fm_UploadHandler.php
+++ fm_UploadHandler.php
@@ -1354,7 +1354,8 @@ class UploadHandler
 						$index,
 						$content_range
 					);
-					if($files){
+				}
+				if($files){
 					@$cut_dir = substr($this->get_upload_path(),58);
 					@$path_foldercutpath = explode('/',$cut_dir);
 					@$show_path = array_values((array_slice($path_foldercutpath, -2)));
@@ -1379,7 +1380,6 @@ class UploadHandler
                                                 'log_action_by' => $_POST['log_user'],
                                                 'log_ip' => $ipshow
                                                 ));
-				}
 				}
 
 			}
