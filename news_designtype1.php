4345643d7442099c52d7e34265e6e8b28c78b148
Puttikun Wongpanyanurak <puttikun@hotmail.com>
2019-05-28 10:57:58 +0700

    test

--- news_designtype1.php
+++ news_designtype1.php
@@ -1,6 +1,7 @@
 <?php
     $id = $_GET['id'];
 	    $rs = $db->findByPK33('news','newsDetails','user','news_newsDetails_id','newsDetails_id','user_user_id','user_id','news_id',$id)->executeAssoc();
+	    print_r($rs);
 ?>
 <div class='col-xl-1 col-lg-1 col-md-1 col-sm-1 col-1'></div>
 <div class='col-xl-10 col-lg-10 col-md-10 col-sm-10 col-10 newborder' style="background-color:#ffffff;">
