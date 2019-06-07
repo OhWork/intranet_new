179a7631e997578d29e86a66b41b1cdd02ee411f
Choatchaw <Choatchaw@gmail.com>
2019-06-07 22:55:34 +0700

    แก้แสดเงผล ให้แสดงรายละเอียด

--- qtn_show_qtnmain.php
+++ qtn_show_qtnmain.php
@@ -14,11 +14,15 @@
 				<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
 					<?php
     					$date = date('Y-m-d H:i');
-                        $rs = $db->specifytable('*','qtn',"qtn_enable = 1 AND '$date' BETWEEN qtn_datestart AND qtn_dateend")->execute();
+                        $rs = $db->specifytable('*','qtn,user,zoo',"qtn.user_user_id = user.user_id AND user.subzoo_zoo_zoo_id = zoo.zoo_id AND qtn_enable = 1 AND '$date' BETWEEN qtn_datestart AND qtn_dateend")->execute();
 						foreach($rs as $rsshow){
 						?>
 						<div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-4" style="padding:16px;float:left;">
-							<a class="btn btn-light col-12 qtnblock shadow" style="background-color:<?php echo $rsshow['qtn_color'];?>;" href="<?php echo $rsshow['qtn_link'];?>" role="button"><?php echo $rsshow['qtn_name'];?></a>
+							<a class="btn btn-light col-12 qtnblock shadow" style="background-color:<?php echo $rsshow['qtn_color'];?>;" href="<?php echo $rsshow['qtn_link'];?>" role="button">
+                                                            <?php echo $rsshow['qtn_name'];?><br>
+                                                            ระหว่างวันที่ <?php echo $rsshow['qtn_datestart'];?> <br>ถึงวันที่ <?php echo $rsshow['qtn_dateend'];?><br>
+                                                            โดย <?php echo $rsshow['zoo_name'];?>
+                                                        </a>
 						</div>
 					<?php }
     					if(empty($rsshow['qtn_id'])){ ?>
