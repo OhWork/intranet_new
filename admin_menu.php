f1aa68ef7bf89f133fc54fdfb90570f65e348d3f
Puttikun Wongpanyanurak <puttikun@hotmail.com>
2019-06-25 22:13:55 +0700

    อัพ

--- admin_menu.php
+++ admin_menu.php
@@ -121,11 +121,6 @@
 			var cutidmenu = idmenushow.substring(1);
 			var idmenu = document.getElementById(cutidmenu);
 			$('.nav-link').attr( 'aria-expanded','false');
-			$( ".sub-menu" ).not(idmenu).removeClass('show');
-/*			var idmenushow = checkde.dataset.target;
-			var cutidmenu = idmenushow.substring(1);
-			var idmenu = document.getElementById(cutidmenu);
-			$('.nav-link').attr( 'aria-expanded','false');
 			if($('.sub-menu').hasClass("show")){
 			 $('.sub-menu').addClass("animat-out");
 				if($('.sub-menu').hasClass("animat-out")){
@@ -153,7 +148,6 @@
 		    }
 		    nav_status = 0 ;
 		    $('.nav-link').removeClass("animat-test");
-*/
 
 	        }else{
 			$('.nav-link').attr( 'aria-expanded','false');
