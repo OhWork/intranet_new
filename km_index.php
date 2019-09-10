11881d7c61078a6befebb68bb2ad069d2a21540e
choatchaw <Choatchaw@live.com>
2019-09-10 20:43:26 +0700

    add

---
+++ km_index.php
@@ -0,0 +1,102 @@
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+        <link rel="stylesheet" href="CSS/bootstrap.css">
+        <link rel="stylesheet" href="CSS/main.css">
+          <title>แหล่งการเรียนรู้</title>
+        <?php
+            include_once 'inc_js.php';
+            include_once 'database/db_tools.php';
+            include_once 'connect.php';
+            include_once 'form/main_form.php';
+            include_once 'form/gridview.php';
+        ?>
+    </head>
+    <body onload="setDefault()">
+        <div class="wrapper">
+        <?php
+$videoID="P-fUlj_Ssq0";
+$videoID2="f4BisN2fyHs";
+$videoID3="d-Bgu2L9R8U";
+$videoID4="sc7Mh8i8jI0";
+$videoID5="K5kZy_S2lFs";
+$videoID6="2JYLqpB-QA0";
+$videoID7="ko4bsIRPx1I";
+$videoID8="_lhePrgym_M";
+$videoID9="rOi1qyuIPYs";
+$videoID10="uXJp9052pYo";
+$videoID11="g-r14ip2jk4";
+?>
+<div class="row">
+    <div class="col-md-12"> <h1>Office the Series</h1> </div>
+<div class="col-md-12">
+    <div class="col-md-4">
+    <h3>ตอนพิเศษ ธ สถิตในดวงใจนิรันดร์</h3>
+    <iframe width="380" height="260" src="//www.youtube.com/embed/<?=$videoID?>?rel=0" frameborder="0" allowfullscreen></iframe>
+    </div>
+    <div class="col-md-4">
+    <h3>ตอนที่ ๑ พ.ร.บ.คอมพิวเตอร์</h3>
+    <iframe width="380" height="260" src="//www.youtube.com/embed/<?=$videoID2?>?rel=0" frameborder="0" allowfullscreen></iframe>
+    </div>
+     <div class="col-md-4">
+     <h3>ตอนที่ ๒ การแก้ปัญหาคอมพิวเตอร์เบื้องต้น</h3>
+    <iframe width="380" height="260" src="//www.youtube.com/embed/<?=$videoID3?>?rel=0" frameborder="0" allowfullscreen></iframe>
+    </div>
+    </div>
+</div>
+<!-- row2 -->
+<div class="col-md-12">
+    <div class="col-md-4">
+        <h3>ตอนที่ ๓ ความแตกต่างระหว่างฮาร์ดแวร์กับซอฟต์แวร์</h3>
+    <iframe width="380" height="260" src="//www.youtube.com/embed/<?=$videoID4?>?rel=0" frameborder="0" allowfullscreen></iframe>
+    </div>
+    <div class="col-md-4">
+    <h3>ตอนที่ ๔ การป้องกันไวรัสคอมพิวเตอร์</h3>
+    <iframe width="380" height="260" src="//www.youtube.com/embed/<?=$videoID5?>?rel=0" frameborder="0" allowfullscreen></iframe>
+
+    </div>
+     <div class="col-md-4">
+    <h3>ตอนที่ ๕ ระบบต่างๆที่ใช้ภายใน อสส.(ระบบทั่วไป)</h3>
+    <iframe width="380" height="260" src="//www.youtube.com/embed/<?=$videoID6?>?rel=0" frameborder="0" allowfullscreen></iframe
+    </div>
+ </div>
+
+     	</div>
+<!-- row3 -->
+<div class="col-md-12">
+    <div class="col-md-4">
+    <h3>ตอนที่ ๖ ระบบต่างๆที่ใช้ใน อสส. (เฉพาะทาง)</h3>
+    <iframe width="380" height="260" src="//www.youtube.com/embed/<?=$videoID7?>?rel=0" frameborder="0" allowfullscreen></iframe>
+    </div>
+    <div class="col-md-4">
+    <h3>ตอนที่ ๗ ประโยชน์และโทษของ Social Media</h3>
+    <iframe width="380" height="260" src="//www.youtube.com/embed/<?=$videoID8?>?rel=0" frameborder="0" allowfullscreen></iframe>
+    </div>
+    
+    <div class="col-md-4">
+    <h3>ตอนที่ ๘ บทบาทและความสำคัญของเทคโนโลยี</h3>
+    <iframe width="380" height="260" src="//www.youtube.com/embed/<?=$videoID9?>?rel=0" frameborder="0" allowfullscreen></iframe>
+    </div>
+
+
+     </div>
+
+     	<!-- row4 -->
+<div class="col-md-12">
+         <div class="col-md-4">
+     <h3>ตอนที่ ๙ ถ้าไม่มีเทคโนโลยีสารสนเทศชีวิตจะเป็นอย่างไร</h3>
+     <iframe width="380" height="260" src="//www.youtube.com/embed/<?=$videoID10?>?rel=0" frameborder="0" allowfullscreen></iframe>
+    </div>
+         <div class="col-md-4">
+    <h3>ตอนที่ ๑๐ การใช้ประโยชน์เทคโนโลยีสารสนเทศในองค์กร</h3>
+    <iframe width="380" height="260" src="//www.youtube.com/embed/<?=$videoID11?>?rel=0" frameborder="0" allowfullscreen></iframe>
+    </div>
+</div>
+
+     	</div>
+
+    </body>
+    <script type="text/javascript">
+    </script>
+
+</html>
