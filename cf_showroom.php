442c5ba144e104d8309b3cd76138e610257642b8
Firore <takbasic722@gmail.com>
2019-09-26 22:12:10 +0700

    ีup

--- cf_showroom.php
+++ cf_showroom.php
@@ -51,20 +51,22 @@
     $cfrs = $db->findByPK('conferroom','conferroom_id',$id)->executeAssoc();
     $conferroom_name = $cfrs['conferroom_name'];?>
 <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 mt-3">
-	<div class="row">
-		<div class='col-xl-1 col-lg-1 col-md-1 col-sm-1 col-1'></div>
-		<div class='col-xl-10 col-lg-10 col-md-10 col-sm-10 col-10'>
-			<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-				<h4><?php echo $conferroom_name ?></h4>
-			</div>
+        <div class="row">
+                <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 card-header">
+                        <div class="row">
+                                <div>
+                                        <h4><?php echo $conferroom_name ?></h4>
+		</div>
 			<nav class="nav nav-tabs" role="tablist" style="margin-top: 16px;width:100%">
 				<a class="nav-item nav-link active" href="#wait" aria-controls="wait" role="tab" data-toggle="tab" style="color:#45B39D;">รอการอนุมัติ</a>
 				<a class="nav-item nav-link" href="#pass" aria-controls="pass" role="tab" data-toggle="tab" style="color:#58D68D;">อนุมัติ</a>
 				<a class="nav-item nav-link" href="#nopass" aria-controls="nopass" role="tab" data-toggle="tab" style="color:#EC7063;">ไม่อนุมัติ</a>
 				<a class="nav-item nav-link" href="#cancelpass" aria-controls="cancelpass" role="tab" data-toggle="tab" style="color:#EC7063;">ยกเลิก</a>
 			</nav>
+                            <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 mt-3 card-body">
+                                        <div class="table-responsive">
 			<div class="tab-content">
-			<div role="tabpanel" class="tab-pane active inf1" id="wait" style="padding-left:16;padding-right:16px;">
+			<div role="tabpanel" class="tab-pane active" id="wait">
 	<?php
 		$columns = array('eventconfer_date','eventconfer_uname','headncf_name','eventconfer_story','eventconfer_psname','eventconfer_start','eventconfer_end','zoo_name');
 		$rs = $db->findByPK45DESC('eventconfer','conferroom','zoo','headncf','confer_confer_id','conferroom_id','conferroom_id',$id,'subzoo_zoo_zoo_id','zoo_id','headncf_headncf_id','headncf_id','eventconfer_status',"'W'",'eventconfer_id')->execute();
@@ -77,7 +79,7 @@
 				$grid->edittxt ='สถานะ';
 				$grid->renderFromDB($columns,$rs);
 	   ?> 	</div>
-			<div role="tabpanel" class="tab-pane inf1" id="pass" style="padding-left:16;padding-right:16px;">
+			<div role="tabpanel" class="tab-pane" id="pass">
 	<?php
 		$columns = array('eventconfer_date','eventconfer_uname','headncf_name','eventconfer_story','eventconfer_psname','eventconfer_start','eventconfer_end','zoo_name');
 		$rs = $db->findByPK45DESC('eventconfer','conferroom','zoo','headncf','confer_confer_id','conferroom_id','conferroom_id',$id,'subzoo_zoo_zoo_id','zoo_id','headncf_headncf_id','headncf_id','eventconfer_status',"'Y'",'eventconfer_id')->execute();
@@ -92,7 +94,7 @@
 				$grid->edittxt ='สถานะ';
 				$grid->renderFromDB($columns,$rs);
 	   ?> 	</div>
-			<div role="tabpanel" class="tab-pane inf1" id="nopass" style="padding-left:16;padding-right:16px;">
+			<div role="tabpanel" class="tab-pane" id="nopass">
 	<?php
 		$columns = array('eventconfer_date','eventconfer_uname','headncf_name','eventconfer_story','eventconfer_psname','eventconfer_start','eventconfer_end','zoo_name');
 		$rs = $db->findByPK45DESC('eventconfer','conferroom','zoo','headncf','confer_confer_id','conferroom_id','conferroom_id',$id,'subzoo_zoo_zoo_id','zoo_id','headncf_headncf_id','headncf_id','eventconfer_status',"'N'",'eventconfer_id')->execute();
@@ -105,7 +107,7 @@
 				$grid->edittxt ='สถานะ';
 				$grid->renderFromDB($columns,$rs);
 	   ?> 	</div>
-			<div role="tabpanel" class="tab-pane inf1" id="cancelpass" style="padding-left:16;padding-right:16px;">
+			<div role="tabpanel" class="tab-pane" id="cancelpass">
 	<?php
 		$columns = array('eventconfer_date','eventconfer_uname','headncf_name','eventconfer_story','eventconfer_psname','eventconfer_start','eventconfer_end','zoo_name');
 		$rs = $db->findByPK45DESC('eventconfer','conferroom','zoo','headncf','confer_confer_id','conferroom_id','conferroom_id',$id,'subzoo_zoo_zoo_id','zoo_id','headncf_headncf_id','headncf_id','eventconfer_status',"'C'",'eventconfer_id')->execute();
@@ -118,9 +120,11 @@
 				$grid->edittxt ='สถานะ';
 				$grid->renderFromDB($columns,$rs);
 	   ?> 	</div>
-			</div>
-		</div>
-		<div class='col-xl-1 col-lg-1 col-md-1 col-sm-1 col-1'></div>
 	</div>
-</div>
+	</div>
+	</div>
+                </div>
+        </div>
+	</div>
+                </div>
 <?php endif; ?>
