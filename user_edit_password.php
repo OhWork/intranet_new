b898e44d5a9e05f17f0ec2b27d1217e934759607
choatchaw <Choatchaw@live.com>
2020-01-06 11:41:28 +0700

    ดำเนินการแก้

---
+++ user_edit_password.php
@@ -0,0 +1,35 @@
+<?php  ob_start();?>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+        <link rel="stylesheet" href="CSS/bootstrap.css">
+        <link rel="stylesheet" href="CSS/main.css">
+	</head>
+<?php
+    include 'database/db_tools.php';
+	include 'connect.php';
+	if(!empty($_POST['user_id'])){
+// 		$data['user_name'] = $_POST['user_name'];
+// 		$data['user_late'] = $_POST['user_late'];
+// 		$data['user_pass'] = $_POST['user_pass'];
+		$data['user_pass'] = $_POST['user_pass'];
+
+
+		$rsfix = $db->update('user',$data,'user_id',$_POST['user_id']);
+
+	}
+
+	if($rs || $rsfix){
+    	if($rs){
+    	    echo "<div class='statusok'>แก้ไขสำเร็จ</div>";
+    	}else if($rsfix){
+            echo "<div class='statusok'>แก้ไขสำเร็จ</div>";
+        }
+            $link = "admin_index.php?url=cs_show_ip.php&id=".$_POST['subzoo_zoo_zoo_id'];
+            header( "Refresh: 2; $link" );
+}
+?>
+</html>
+<?php
+ob_end_flush();
+?>
---
+++ user_show_editpass.php
@@ -0,0 +1,55 @@
+<script>
+            $(document).ready(function() {
+
+                $('#table').DataTable( {
+                "ordering": false,
+                "lengthMenu": [[10, 25, 50, -1], [10, 25, 50, "ทุกหน้า"]],
+                "language": {
+                    "lengthMenu": "แสดง _MENU_ แถวต่อหน้า",
+                    "zeroRecords": "ไม่พบข้อมูล",
+                    "info": "แสดงหน้าที่ _PAGE_ ถึง _PAGES_",
+                    "infoEmpty": "ไม่พบข้อมูล",
+                    "infoFiltered": "(filtered from _MAX_ total records)"
+                }
+    } );
+} );
+</script>
+<?php
+    if (!empty($_SESSION['user_name'])):
+    $columns = array('user_user','user_name','user_last','subzoo_name','zoo_name');
+    $row = "<div class='row'>";
+    $rowend = "</div>";
+    $form = new form();
+    $labelsearchipzpo = new label('ค้นหา');
+
+			$rs2 = "user,subzoo,zoo  where ipzpo.subzoo_subzoo_id = subzoo.subzoo_id && subzoo.zoo_zoo_id = zoo.zoo_id ";
+            $rs = $db->findByPK32DESC('user','subzoo','zoo','user.subzoo_subzoo_id','subzoo.subzoo_id','subzoo.zoo_zoo_id','zoo.zoo_id','user_id')->execute();  ?> 
+<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 mt-3">
+        <div class="row">
+                <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 card-header">
+                        <div class="row">
+                                <div>
+                                        <h4>รายชื่อผู้ใช้ระบบ</h4>
+		</div>
+		<div class="ml-auto">
+                                        <a href="admin_index.php?url=user_editpass.php" class="btn btn-success col-12"><span data-feather="user-plus"></span></a>
+		</div>
+		<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 mt-3 card-body">
+                                        <div class="table-responsive">
+			<?php 
+                                                        $grid = new gridView();
+                                                                    $grid->pr = 'user_id';
+                                                                    $grid->header = array('<b><center>User</center></b>','<b><center>ชื่อ</center></b>','<b><center>นามสกุล</center></b>','<b><center>ฝ่าย</center></b>','<b><center>สังกัด</center></b>','<b><center>#</center></b>');
+                                                                    $grid->width = array('12%','15%','15%','25%','25%','8%');
+                                                                    $grid->edit = 'admin_index.php?url=user_add_user.php';
+                                                                    $grid->name = 'table';
+                                                                    $grid->edittxt ='แก้ไข';
+                                                                    $grid->renderFromDB($columns,$rs);
+                                                                    endif;
+                                                            ?>
+                                        </div>
+		</div>
+                        </div>
+	</div>
+        </div>
+</div>
