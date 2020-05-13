ba66a74f45ac6839073c73a17f3419f530e9ac76
choatchaw <Choatchaw@live.com>
2020-05-13 22:11:40 +0700

    แก้ไง

--- yt_index.php
+++ yt_index.php
@@ -1,3 +1,180 @@
-<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 alltxh" style="color:#d14;margin: auto;margin-top:300px;margin-bottom:300px;">
-    <h1>อยู่ระหว่างดำเนินการปรับปรุง ของสำนักดิจิทัลและสารสนเทศ</h1>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+        <link rel="stylesheet" href="CSS/bootstrap.css">
+        <link rel="stylesheet" href="CSS/main.css">
+          <title>สื่อองค์การสวนสัตว์</title>
+        <?php
+            include_once 'inc_js.php';
+            include_once 'database/db_tools.php';
+            include_once 'connect.php';
+            include_once 'form/main_form.php';
+            include_once 'form/gridview.php';
+        ?>
+    </head>
+    <body onload="setDefault()">
+        <div class="wrapper">
+        <?php
+$videoID="P-fUlj_Ssq0";
+$videoID2="f4BisN2fyHs";
+$videoID3="d-Bgu2L9R8U";
+$videoID4="sc7Mh8i8jI0";
+$videoID5="K5kZy_S2lFs";
+$videoID6="2JYLqpB-QA0";
+$videoID7="ko4bsIRPx1I";
+$videoID8="_lhePrgym_M";
+$videoID9="rOi1qyuIPYs";
+$videoID10="uXJp9052pYo";
+$videoID11="g-r14ip2jk4";
+?>
+ <div class="row">
+        <div class="col-12 mt-3"> 
+                <h3>สื่อประชาสัมพันธ์</h3> 
+        </div>
+        <div class="col-12 mt-3">
+                <div class="row">
+                        <div class="col-4">
+                               <div class="col-12">
+                                        <h5>สัตว์ทำยังไง?! สวนสัตว์ปิดโควิด | Zoo During Covid Lockdown</h5>
+                                </div>
+                                <div class="col-12">
+                                        <iframe width="380" height="260" src="//www.youtube.com/embed/yVJc1O1U96c?rel=0" frameborder="0" allowfullscreen></iframe>
+                                </div>
+                </div>
+                                            <div class="col-4">
+                                <div class="col-12">
+                                        <h5></h5>
+                                </div>
+                                <div class="col-12">
+                                        <iframe width="380" height="260" src="//www.youtube.com/embed/<?=$videoID2?>?rel=0" frameborder="0" allowfullscreen></iframe>
+                                </div>
+                        </div>
+                        <div class="col-4">
+                                <div class="col-12">
+                                        <h5></h5>
+                                </div>
+                                <div class="col-12">
+                                        <iframe width="380" height="260" src="//www.youtube.com/embed/<?=$videoID3?>?rel=0" frameborder="0" allowfullscreen></iframe>
+                                </div>
+                        </div>
+        </div>
+        </div>
+ </div>
+<div class="row">
+        <div class="col-12 mt-3"> 
+                <h3>Office the Series</h3> 
+        </div>
+        <div class="col-12 mt-3">
+                <div class="row">
+                        <div class="col-4">
+                                <div class="col-12">
+                                        <h5>ตอนพิเศษ ธ สถิตในดวงใจนิรันดร์</h5>
+                                </div>
+                                <div class="col-12">
+                                        <iframe width="380" height="260" src="//www.youtube.com/embed/<?=$videoID?>?rel=0" frameborder="0" allowfullscreen></iframe>
+                                </div>
+                        </div>
+                        <div class="col-4">
+                                <div class="col-12">
+                                        <h5>ตอนที่ ๑ พ.ร.บ.คอมพิวเตอร์</h5>
+                                </div>
+                                <div class="col-12">
+                                        <iframe width="380" height="260" src="//www.youtube.com/embed/<?=$videoID2?>?rel=0" frameborder="0" allowfullscreen></iframe>
+                                </div>
+                        </div>
+                        <div class="col-4">
+                                <div class="col-12">
+                                        <h5>ตอนที่ ๒ การแก้ปัญหาคอมพิวเตอร์เบื้องต้น</h5>
+                                </div>
+                                <div class="col-12">
+                                        <iframe width="380" height="260" src="//www.youtube.com/embed/<?=$videoID3?>?rel=0" frameborder="0" allowfullscreen></iframe>
+                                </div>
+                        </div>
+                </div>
+        </div>
+<!-- row2 -->
+        <div class="col-12">
+                <div class="row">
+                        <div class="col-4">
+                                <div class="col-12">
+                                        <h5>ตอนที่ ๓ ความแตกต่างระหว่างฮาร์ดแวร์กับซอฟต์แวร์</h5>
+                                </div>
+                                <div class="col-12">
+                                        <iframe width="380" height="260" src="//www.youtube.com/embed/<?=$videoID4?>?rel=0" frameborder="0" allowfullscreen></iframe>
+                                </div>
+                        </div>
+                        <div class="col-4">
+                                <div class="col-12">
+                                        <h5>ตอนที่ ๔ การป้องกันไวรัสคอมพิวเตอร์</h5>
+                                </div>
+                                <div class="col-12">
+                                        <iframe width="380" height="260" src="//www.youtube.com/embed/<?=$videoID5?>?rel=0" frameborder="0" allowfullscreen></iframe>
+                                </div>
+                        </div>
+                        <div class="col-4">
+                                <div class="col-12">
+                                        <h5>ตอนที่ ๕ ระบบต่างๆที่ใช้ภายใน อสส.(ระบบทั่วไป)</h5>
+                                </div>
+                                <div class="col-12">
+                                    <iframe width="380" height="260" src="//www.youtube.com/embed/<?=$videoID6?>?rel=0" frameborder="0" allowfullscreen></iframe>
+                                </div>
+                        </div>
+                </div>
+        </div>
+<!-- row3 -->
+        <div class="col-12">
+                <div class="row">
+                        <div class="col-4">
+                                <div class="col-12">
+                                        <h5>ตอนที่ ๖ ระบบต่างๆที่ใช้ใน อสส. (เฉพาะทาง)</h5>
+                                </div>
+                                <div class="col-12">
+                                        <iframe width="380" height="260" src="//www.youtube.com/embed/<?=$videoID7?>?rel=0" frameborder="0" allowfullscreen></iframe>
+                                </div>
+                        </div>
+                        <div class="col-4">
+                                <div class="col-12">
+                                        <h5>ตอนที่ ๗ ประโยชน์และโทษของ Social Media</h5>
+                                </div>
+                                <div class="col-12">
+                                        <iframe width="380" height="260" src="//www.youtube.com/embed/<?=$videoID8?>?rel=0" frameborder="0" allowfullscreen></iframe>
+                                </div>
+                        </div>
+                        <div class="col-4">
+                                <div class="col-12">
+                                        <h5>ตอนที่ ๘ บทบาทและความสำคัญของเทคโนโลยี</h5>
+                                </div>
+                                <div class="col-12">
+                                        <iframe width="380" height="260" src="//www.youtube.com/embed/<?=$videoID9?>?rel=0" frameborder="0" allowfullscreen></iframe>
+                                </div>
+                        </div>
+                </div>
+        </div>
+     	<!-- row4 -->
+        <div class="col-12">
+                <div class="row">
+                        <div class="col-4">
+                                <div class="col-12">
+                                        <h5>ตอนที่ ๙ ถ้าไม่มีเทคโนโลยีสารสนเทศชีวิตจะเป็นอย่างไร</h5>
+                                </div>
+                                <div class="col-12">
+                                        <iframe width="380" height="260" src="//www.youtube.com/embed/<?=$videoID10?>?rel=0" frameborder="0" allowfullscreen></iframe>
+                                </div>
+                        </div>
+                        <div class="col-4">
+                                <div class="col-12">
+                                        <h5>ตอนที่ ๑๐ การใช้ประโยชน์เทคโนโลยีสารสนเทศในองค์กร</h5>
+                                </div>
+                                <div class="col-12">
+                                        <iframe width="380" height="260" src="//www.youtube.com/embed/<?=$videoID11?>?rel=0" frameborder="0" allowfullscreen></iframe>
+                                </div>
+                        </div>
+                </div>
+        </div>
 </div>
+    
+    <script type="text/javascript">
+    </script>
+</div>
+        </body>
+</html>
