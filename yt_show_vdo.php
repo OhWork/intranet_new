cd9866637e50f93f2b8684ce736f85636a3b09dd
choatchaw <Choatchaw@live.com>
2020-05-14 22:33:30 +0700

    youtube

---
+++ yt_show_vdo.php
@@ -0,0 +1,36 @@
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+        <link rel="stylesheet" href="CSS/bootstrap.css">
+        <link rel="stylesheet" href="CSS/main.css">
+          <title>สื่อองค์การสวนสัตว์</title>
+        <?php
+            include_once 'inc_js.php';
+            include_once 'database/db_tools.php';
+            include_once 'connect.php';
+            include_once 'form/main_form.php';
+            include_once 'form/gridview.php';
+        ?>
+    </head>
+    <body onload="setDefault()">
+        <div class="wrapper">
+        <div class="col-12 mt-3">
+                <div class="row">
+                        <div class="col-4">
+                               <div class="col-12">
+                                        <h5>สัตว์ทำยังไง?! สวนสัตว์ปิดโควิด | Zoo During Covid Lockdown</h5>
+                                </div>
+                                <div class="col-12">
+                                        <iframe width="380" height="260" src="//www.youtube.com/embed/yVJc1O1U96c?rel=0" frameborder="0" allowfullscreen></iframe>
+                                </div>
+                </div>
+
+        </div>
+        </div>
+ </div>
+
+    
+    <script type="text/javascript">
+    </script>
+        </body>
+</html>
---
+++ yt_show_vdo_all.php
@@ -0,0 +1,37 @@
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+        <link rel="stylesheet" href="CSS/bootstrap.css">
+        <link rel="stylesheet" href="CSS/main.css">
+          <title>สื่อองค์การสวนสัตว์</title>
+        <?php
+            include_once 'inc_js.php';
+            include_once 'database/db_tools.php';
+            include_once 'connect.php';
+            include_once 'form/main_form.php';
+            include_once 'form/gridview.php';
+            $yt = "QyWBnRyODuE";
+        ?>
+    </head>
+    <body onload="setDefault()">
+        <div class="wrapper">
+        <div class="col-12 mt-3">
+                <div class="row">
+                        <div class="col-4">
+                               <div class="col-12">
+                                        <h5>สัตว์ทำยังไง?! สวนสัตว์ปิดโควิด | Zoo During Covid Lockdown</h5>
+                                </div>
+                                <div class="col-12">
+                                        <img src=<?php echo "https://img.youtube.com/vi/$yt/0.jpg"; ?> />
+                                </div>
+                </div>
+
+        </div>
+        </div>
+ </div>
+
+    
+    <script type="text/javascript">
+    </script>
+        </body>
+</html>
