13f2d5dc4d8913453b9ca28e9c8219d1fa130840
Firore <takbasic722@gmail.com>
2020-05-18 22:09:27 +0700

    up ของกูด้วย

---
+++ yt_play_vdo.php
@@ -0,0 +1,14 @@
+<div class="row">
+    <div class="col-12 mt-5"> 
+        <h3>สัตว์ทำยังไง?! สวนสัตว์ปิดเพราะโควิด 2019 | Zoo During Covid Lockdown</h3> 
+    </div>
+    <div class="col-12 mt-5 mb-5">
+        <div class="row">
+            <div class="col-3"></div>
+            <div class="col-6">
+                <iframe width="100%" height="400" src="//www.youtube.com/embed/yVJc1O1U96c?rel=0" frameborder="0" allowfullscreen></iframe>
+            </div>
+            <div class="col-3"></div>
+        </div>
+    </div>   
+</div>
