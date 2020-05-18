39ed4113610729104b20968ba796c79d7ddccbf9
choatchaw <Choatchaw@live.com>
2020-05-18 23:18:35 +0700

    oik

--- yt_index.php
+++ yt_index.php
@@ -1,3 +1,6 @@
+<?php
+ 	$rs = $db->findByPK21DESC('vdo','user','user_user_id','user_id','vdo_id')->execute();
+?>
 <div class="row">
     <div class="col-12 mt-3"> 
         <h3>สื่อวีดีทัศน์</h3> 
@@ -9,84 +12,21 @@
             </div>
             <div class="col-12">
                 <div class="row">
-                    <a class="col-2" href="index.php?url=yt_play_vdo.php"> <!-- VDO 1-->
+                    <?php $i = 0; foreach($rs as $show){ ?>
+                    <a class="col-2" href="index.php?url=yt_play_vdo.php&id=<?php echo $show['vdo_id']; ?>"> <!-- VDO 1-->
                         <div class="col-12">
                             <div class="row">
                                 <!--<iframe width="100%" height="120" src="//www.youtube.com/embed/yVJc1O1U96c?rel=0" frameborder="0" allowfullscreen></iframe>-->
-                                <img width="100%" height="120" src="images/no_pic.png">
+                                <img width="100%" height="120" src="https://img.youtube.com/vi/<?php echo $show['vdo_link']; ?>/0.jpg">
                             </div>
                         </div>
                         <div class="col-12" style="">
                             <div class="row">
-                                <p style="margin-bottom: 0px;white-space: nowrap;width: 200px;overflow: hidden;text-overflow: ellipsis;height:40px;line-height:50px;" title="สัตว์ทำยังไง?! สวนสัตว์ปิดเพราะโควิด 2019 | Zoo During Covid Lockdown">สัตว์ทำยังไง?! สวนสัตว์ปิดเพราะโควิด 2019 | Zoo During Covid Lockdown</p>
+                                <p style="margin-bottom: 0px;white-space: nowrap;width: 200px;overflow: hidden;text-overflow: ellipsis;height:40px;line-height:50px;"><?php echo $show['vdo_name']; ?></p>
                             </div>
                         </div>
                     </a>
-                    <a class="col-2" href="index.php?url=yt_play_vdo.php"> <!-- VDO 2-->
-                        <div class="col-12">
-                            <div class="row">
-                                <!--<iframe width="100%" height="120" src="//www.youtube.com/embed/yVJc1O1U96c?rel=0" frameborder="0" allowfullscreen></iframe>-->
-                                <img width="100%" height="120" src="images/no_pic.png">
-                            </div>
-                        </div>
-                        <div class="col-12" style="">
-                            <div class="row">
-                                <p style="margin-bottom: 0px;white-space: nowrap;width: 200px;overflow: hidden;text-overflow: ellipsis;height:40px;line-height:50px;" title="สัตว์ทำยังไง?! สวนสัตว์ปิดเพราะโควิด 2019 | Zoo During Covid Lockdown">สัตว์ทำยังไง?! สวนสัตว์ปิดเพราะโควิด 2019 | Zoo During Covid Lockdown</p>
-                            </div>
-                        </div>
-                    </a>
-                    <a class="col-2" href="index.php?url=yt_play_vdo.php"> <!-- VDO 3-->
-                        <div class="col-12">
-                            <div class="row">
-                                <!--<iframe width="100%" height="120" src="//www.youtube.com/embed/yVJc1O1U96c?rel=0" frameborder="0" allowfullscreen></iframe>-->
-                                <img width="100%" height="120" src="images/no_pic.png">
-                            </div>
-                        </div>
-                        <div class="col-12" style="">
-                            <div class="row">
-                                <p style="margin-bottom: 0px;white-space: nowrap;width: 200px;overflow: hidden;text-overflow: ellipsis;height:40px;line-height:50px;" title="สัตว์ทำยังไง?! สวนสัตว์ปิดเพราะโควิด 2019 | Zoo During Covid Lockdown">สัตว์ทำยังไง?! สวนสัตว์ปิดเพราะโควิด 2019 | Zoo During Covid Lockdown</p>
-                            </div>
-                        </div>
-                    </a>
-                    <a class="col-2" href="index.php?url=yt_play_vdo.php"> <!-- VDO 4-->
-                        <div class="col-12">
-                            <div class="row">
-                                <!--<iframe width="100%" height="120" src="//www.youtube.com/embed/yVJc1O1U96c?rel=0" frameborder="0" allowfullscreen></iframe>-->
-                                <img width="100%" height="120" src="images/no_pic.png">
-                            </div>
-                        </div>
-                        <div class="col-12" style="">
-                            <div class="row">
-                                <p style="margin-bottom: 0px;white-space: nowrap;width: 200px;overflow: hidden;text-overflow: ellipsis;height:40px;line-height:50px;" title="สัตว์ทำยังไง?! สวนสัตว์ปิดเพราะโควิด 2019 | Zoo During Covid Lockdown">สัตว์ทำยังไง?! สวนสัตว์ปิดเพราะโควิด 2019 | Zoo During Covid Lockdown</p>
-                            </div>
-                        </div>
-                    </a>
-                    <a class="col-2" href="index.php?url=yt_play_vdo.php"> <!-- VDO 5-->
-                        <div class="col-12">
-                            <div class="row">
-                                <!--<iframe width="100%" height="120" src="//www.youtube.com/embed/yVJc1O1U96c?rel=0" frameborder="0" allowfullscreen></iframe>-->
-                                <img width="100%" height="120" src="images/no_pic.png">
-                            </div>
-                        </div>
-                        <div class="col-12" style="">
-                            <div class="row">
-                                <p style="margin-bottom: 0px;white-space: nowrap;width: 200px;overflow: hidden;text-overflow: ellipsis;height:40px;line-height:50px;" title="สัตว์ทำยังไง?! สวนสัตว์ปิดเพราะโควิด 2019 | Zoo During Covid Lockdown">สัตว์ทำยังไง?! สวนสัตว์ปิดเพราะโควิด 2019 | Zoo During Covid Lockdown</p>
-                            </div>
-                        </div>
-                    </a>
-                    <a class="col-2" href="index.php?url=yt_play_vdo.php"> <!-- VDO 6-->
-                        <div class="col-12">
-                            <div class="row">
-                                <!--<iframe width="100%" height="120" src="//www.youtube.com/embed/yVJc1O1U96c?rel=0" frameborder="0" allowfullscreen></iframe>-->
-                                <img width="100%" height="120" src="images/no_pic.png">
-                            </div>
-                        </div>
-                        <div class="col-12" style="">
-                            <div class="row">
-                                <p style="margin-bottom: 0px;white-space: nowrap;width: 200px;overflow: hidden;text-overflow: ellipsis;height:40px;line-height:50px;" title="สัตว์ทำยังไง?! สวนสัตว์ปิดเพราะโควิด 2019 | Zoo During Covid Lockdown">สัตว์ทำยังไง?! สวนสัตว์ปิดเพราะโควิด 2019 | Zoo During Covid Lockdown</p>
-                            </div>
-                        </div>
-                    </a>
+                    <?php   $i++; } ?>
                 </div>
             </div>
             <div class="col-12 mt-1"> 
--- yt_play_vdo.php
+++ yt_play_vdo.php
@@ -1,12 +1,16 @@
+<?php 
+            $id = $_GET['id'];
+            $rs = $db->findByPK22('vdo','user','user_user_id','user_id','vdo_id',$id)->executeAssoc();
+?>
 <div class="row">
     <div class="col-12 mt-5"> 
-        <h3>สัตว์ทำยังไง?! สวนสัตว์ปิดเพราะโควิด 2019 | Zoo During Covid Lockdown</h3> 
+        <h3><?php echo $rs['vdo_name']; ?></h3> 
     </div>
     <div class="col-12 mt-5 mb-5">
         <div class="row">
             <div class="col-3"></div>
             <div class="col-6">
-                <iframe width="100%" height="400" src="//www.youtube.com/embed/yVJc1O1U96c?rel=0" frameborder="0" allowfullscreen></iframe>
+                <iframe width="100%" height="400" src="//www.youtube.com/embed/<?php echo $rs['vdo_link']; ?>" frameborder="0" allowfullscreen></iframe>
             </div>
             <div class="col-3"></div>
         </div>
