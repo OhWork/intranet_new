97e3b2ce24ad6bbdc7af31705ca173e0722dab13
choatchaw <Choatchaw@live.com>
2020-07-08 22:07:32 +0700

    aaa

---
+++ zlfot_insert_renew.php
@@ -0,0 +1,83 @@
+<?php  ob_start();?>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+        <link rel="stylesheet" href="CSS/bootstrap.css">
+        <link rel="stylesheet" href="CSS/main.css">
+	</head>
+<?php
+    include 'database/db_tools.php';
+	include 'connect.php';
+        $typezlfot =  $_POST['typezlfot_typezlfot_id'];
+        $user_id = $_POST['user_user_id'];
+        $typezooexe = $db->findByPK22("user","zoo","user.subzoo_zoo_zoo_id","zoo.zoo_id","user.user_id",$user_id)->executeAssoc();
+        if($typezlfot == '01' || $typezlfot ==  '02'){
+        $datenow = date("Y");   
+        $datenow.="-".date("m");   
+        $datenow.= "-".date("d"); 
+        $dateend = substr((date("Y")+1),0);   
+        $dateend.="-".date("m");   
+        $dateend.= "-".date("d"); 
+        }else if( $typezlfot == '03'){
+        $datenow = date("Y");   
+        $datenow.="-".date("m");   
+        $datenow.= "-".date("d"); 
+        $dateend = substr((date("Y")+3),0);   
+        $dateend.="-".date("m");   
+        $dateend.= "-".date("d"); 
+        }else{
+            $datenow = "ฟรี";
+            $dateend = "ตลอดชีวิต";
+        }
+       $typezoo =  $typezooexe['zoo_code'];
+        $codezlfot = $typezoo.$typezlfot;
+        $rs = $db->specifytable("MAX(zlfot_code) AS last_id","zlfot","zlfot_code LIKE '%$codezlfot%'")->executeAssoc();
+        $maxId = substr($rs['last_id'],  -5);
+        $maxId = ($maxId + 1); 
+        $maxId = substr("00000".$maxId, -5);
+        $nextId = $codezlfot.$maxId;
+
+        
+	if(!empty($_POST['subzoo_id'])){
+		$data['subzoo_name'] = $_POST['subzoo_name'];
+		$data['subzoo_year'] = $_POST['subzoo_year'];
+		$data['subzoo_no'] = $_POST['subzoo_no'];
+		$data['subzoo_detail'] = $_POST['subzoo_detail'];
+		$data['subzoo_enable'] = $_POST['subzoo_enable'];
+		$rsfix = $db->update('subzoo',$data,'subzoo_id',$_POST['subzoo_id']);
+
+	}else{
+	$rs = $db->insert('zlfot',array(
+	'zlfot_code' => $nextId,
+	'zlfot_nameth' => $_POST['zlfot_nameth'],
+	'zlfot_nameen' => $_POST['zlfot_nameen'],
+                'zlfot_tel' => $_POST['zlfot_tel'],
+               	'zlfot_address' => $_POST['zlfot_address'],
+                'zlfot_receipt' => $_POST['zlfot_receipt'],
+	'zlfot_datereg' => $_POST['zlfot_datereg'],
+                'zlfot_datestart' => $datenow,
+                'zlfot_dateend' => $dateend,
+                'zlfot_status' => $_POST['zlfot_status'],
+                'zlfot_email' => $_POST['zlfot_email'],
+                'zlfot_detail' => $_POST['zlfot_detail'],
+	'typezlfot_typezlfot_id' => $_POST['typezlfot_typezlfot_id'],
+	'user_user_id' => $_POST['user_user_id']
+	));
+	}
+
+	if($rs || $rsfix){
+    	if($rs){
+    	    echo "<div class='statusok'>เพิ่มสำเร็จ</div>";
+    	}else if($rsfix){
+            echo "<div class='statusok'>แก้ไขสำเร็จ</div>";
+        }
+            $link = "url=admin_index.php?url=zlfot_show_member.php";
+            header( "Refresh: 2; $link" );
+}else{
+            echo "ข้อมูลไม่เข้าฐานข้อมูล";
+        }
+?>
+</html>
+<?php
+ob_end_flush();
+?>
--- zlfot_renew.php
+++ zlfot_renew.php
@@ -2,8 +2,14 @@
     if (!empty($_SESSION['user_name'])):
     $id =  $_GET['id'];
     $form = new form();
-    $button = new buttonok("ยืนยันการตรวจสอบ","","btn btn-success btn-block col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12","");
-    $button2 = new buttonok("แก้ไขข้อมูล","","btn btn-warning btn-block col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12","");
+    $lbtype = new label('ประเภทสมาชิก'); 
+    $radiotypezlfot = new radioGroup();
+    $radiotypezlfot->name = 'typezlfot_typezlfot_id';
+           $rstype = $db->findAll('typezlfot')->execute();
+           foreach($rstype as $type){
+    	$radiotypezlfot->add($type['typezlfot_name'],$type['typezlfot_code'],'','');
+           }
+    $button = new buttonok("ต่ออายุ","","btn btn-success btn-block col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12","");
     $rs = $db->findByPK44('zlfot','typezlfot','user','zoo','zlfot.user_user_id','user.user_id','zlfot.typezlfot_typezlfot_id','typezlfot.typezlfot_id','user.subzoo_zoo_zoo_id','zoo.zoo_id','zlfot_id',$id)->executeRow();
     echo $form->open("form_reg","frmMain","col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 mt-3","zlfot_insert_updatestatus.php","");
 	?>
@@ -13,7 +19,7 @@
 			<div class="col-xl-8 col-lg-8 col-md-10 col-sm-12 col-12" style="padding-top:16px;background-color:#ffffff;border:solid 1px #E0E0E0;border-radius:7px;font-size: 18px;">
                                         <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 mb-4">
 						<div class="row">
-                                                    <h2>รอการตรวจสอบความถูกต้อง</h2>
+                                                    <h2>ตรวจสอบความถูกต้อง</h2>
 						</div>
 					</div>
 					<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
@@ -69,16 +75,6 @@
 					<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
 						<div class="row">
 							<div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-12">
-								<p>วันที่ลงทะเบียนเข้าสู่ระบบ</p>
-							</div>
-							<div class="col-xl-8 col-lg-8 col-md-6 col-sm-12 col-12">
-								<?php echo $rs['zlfot_datereg']; ?>
-							</div>
-						</div>
-					</div>
-					<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-						<div class="row">
-							<div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-12">
 								<p>วันที่สมัคร</p>
 							</div>
 							<div class="col-xl-8 col-lg-8 col-md-6 col-sm-12 col-12">
@@ -108,25 +104,21 @@
 					</div>
 					<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 mb-3">
 						<div class="row">
-							<div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-12">
-								<p>หน่วยงาน</p>
-							</div>
-							<div class="col-xl-8 col-lg-8 col-md-6 col-sm-12 col-12">
-								<?php echo $rs['zoo_name']; ?>
-							</div>
+                                                                <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 mt-1 showmsg">
+					<?php echo $lbtype; ?>
+				</div>
+				<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 showmsg">
+					<?php echo $radiotypezlfot;?>
+				</div>
 						</div>
 					</div>
 					<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 mb-4">
 						<div class="row">
-							<input type='hidden' name='zlfot_status' value='P'> 
 							<input type='hidden' name='zlfot_id' value=<?php echo $id; ?>> 
 							<div class="col-xl-3 col-lg-2 col-md-2"></div>
 							<div class="col-xl-3 col-lg-4 col-md-4 col-sm-12 col-12">
                                                             <?php echo $button; ?>
 							</div>
-                                                        <div class="col-xl-3 col-lg-4 col-md-4">
-                                                            <?php echo $button2; ?>
-                                                        </div>
                                                         <div class="col-xl-3 col-lg-2 col-md-2"></div>
 						</div>
 					</div>
