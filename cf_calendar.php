efdd1835836c0a79bbc7fa0a670f1d386c12ccd0
choatchaw <Choatchaw@live.com>
2020-07-21 20:55:10 +0700

    Update cf_calendar.php

--- cf_calendar.php
+++ cf_calendar.php
@@ -1,7 +1,7 @@
 <?php
 	$id = $_GET['id'];
 
-	$r = $db->findByPK('conferroom','conferroom_id',$id)->executeRow();
+	$r = $db->findByPKLimit('conferroom','conferroom_id',$id,100)->executeRow();
 	$confer = $r['conferroom_name'];
 ?>
 <!DOCTYPE html>
