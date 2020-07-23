e89a477eb492562ce225fdaa2bc617a0292e465f
choatchaw <Choatchaw@live.com>
2020-07-23 21:36:29 +0700

    Update zlfot_insert_card.php

--- zlfot_insert_card.php
+++ zlfot_insert_card.php
@@ -125,7 +125,7 @@
             echo "<div class='statusok'>ต่ออายุสำเร็จ</div>";
            }
             $link = "url=admin_index.php?url=zlfot_show_checkmember.php&type=1";
-            //header( "Refresh: 2; $link" );
+            header( "Refresh: 2; $link" );
         }else{
             echo "ข้อมูลไม่เข้าฐานข้อมูล";
         }
