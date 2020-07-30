67d870b350b2b31ecb7d17a436ce0030eba8db82
choatchaw <Choatchaw@live.com>
2020-07-30 20:44:27 +0700

    Update zlfot_insert_updatestatus.php

--- zlfot_insert_updatestatus.php
+++ zlfot_insert_updatestatus.php
@@ -9,10 +9,46 @@
 <?php
     include 'database/db_tools.php';
 	include 'connect.php';
-	if(!empty($_POST['zlfotcard_id'])){          
-                         $data['zlfotcard_status'] = $_POST['zlfotcard_status'];
-                        $data['zlfotcard_stsfw'] = $_POST['zlfotcard_stsfw'];
-                        $rsfix = $db->update('zlfotcard',$data,'zlfotcard_id',$_POST['zlfotcard_id']);
+	if(!empty($_POST['zlfotcard_id'])){
+                                        
+             switch ($_POST['zlfotcard_stsfw']){
+                          case "P":
+                               $data['zlfotcard_status'] = $_POST['zlfotcard_status'];
+                               $data['zlfotcard_stsfw'] = $_POST['zlfotcard_stsfw'];
+                               $rsfix = $db->update('zlfotcard',$data,'zlfotcard_id',$_POST['zlfotcard_id']);
+                            break;
+                          case "S":
+                            $data['zlfotcard_stsfw'] = $_POST['zlfotcard_stsfw'];
+                            $rsfix = $db->update('zlfotcard',$data,'zlfotcard_id',$_POST['zlfotcard_id']);
+                            break;
+                          case "T":
+                               $data['zlfotcard_stsfw'] = $_POST['zlfotcard_stsfw'];
+                               $data['sendcard_sendcard_id'] = $_POST['zlfotmember_id'];
+                               $rsfix = $db->update('zlfotcard',$data,'zlfotcard_id',$_POST['zlfotcard_id']);
+                               if($_POST['sendcard_status'] =='Y'){
+                                           $db->insert('sendcard',array(
+                                                    'sendcard_code' => $_POST['sendcard_code'],
+                                                    'sendcard_status' => $_POST['sendcard_status'],
+                                                    'sendcard_date' => $_POST['sendcard_date'],
+                                                     'zlfotmember_zlfotmember_id' => $_POST['zlfotmember_id'],
+                                                    'postoffice_postoffice_id' => $_POST['postoffice_postoffice_id']
+                                                    ));
+                                          }else if($_POST['sendcard_status'] =='N'){
+                                            $db->insert('sendcard',array(
+                                                    'sendcard_status' => $_POST['sendcard_status'],
+                                                    'sendcard_date' => $_POST['sendcard_date'],
+                                                    'zlfotmember_zlfotmember_id' => $_POST['zlfotmember_id'],
+                                                    'postoffice_postoffice_id' => $_POST['postoffice_postoffice_id']
+                                                    ));
+                                        }
+                            break;
+                           case "C":
+                            $data['zlfotcard_receiptfin'] = $_POST['zlfotcard_receiptfin'];
+                            $data['zlfotcard_stsfw'] = $_POST['zlfotcard_stsfw'];
+                            $rsfix = $db->update('zlfotcard',$data,'zlfotcard_id',$_POST['zlfotcard_id']);
+                            break;
+                    }
+            
             
           
             //Log
@@ -39,7 +75,20 @@
     	}else if($rsfix){
             echo "<div class='statusok'>แก้ไขสำเร็จ</div>";
                     }
+                    switch ($_POST['zlfotcard_stsfw']){
+                          case "P":
                              $link = "admin_index.php?url=zlfot_show_checkmember.php&type=2";
+                            break;
+                          case "S":
+                            $link = "admin_index.php?url=zlfot_show_checkmember.php&type=3";
+                            break;
+                          case "T":
+                            $link = "admin_index.php?url=zlfot_show_checkmember.php&type=4";
+                            break;
+                           case "C":
+                            $link = "admin_index.php?url=zlfot_show_member.php";
+                            break;
+                    }
             header( "Refresh: 2; $link" );
 }
 ?>
