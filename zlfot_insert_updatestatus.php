3c46367426966387af90c7f31594421b7bd9bdaf
choatchaw <Choatchaw@live.com>
2020-07-30 20:43:28 +0700

    a

--- zlfot_insert_updatestatus.php
+++ zlfot_insert_updatestatus.php
@@ -9,35 +9,8 @@
 <?php
     include 'database/db_tools.php';
 	include 'connect.php';
-	if(!empty($_POST['zlfotcard_id'])){
-                                        
-             switch ($_POST['zlfotcard_stsfw']){
-                          case "P":
-                               $data['zlfotcard_status'] = $_POST['zlfotcard_status'];
-                            break;
-                          case "T":
-                               $data['sendcard_sendcard_id'] = $_POST['zlfotmember_id'];
-                               if($_POST['sendcard_status'] =='Y'){
-                                           $db->insert('sendcard',array(
-                                                    'sendcard_code' => $_POST['sendcard_code'],
-                                                    'sendcard_status' => $_POST['sendcard_status'],
-                                                    'sendcard_date' => $_POST['sendcard_date'],
-                                                     'zlfotmember_zlfotmember_id' => $_POST['zlfotmember_id'],
-                                                    'postoffice_postoffice_id' => $_POST['postoffice_postoffice_id']
-                                                    ));
-                                          }else if($_POST['sendcard_status'] =='N'){
-                                            $db->insert('sendcard',array(
-                                                    'sendcard_status' => $_POST['sendcard_status'],
-                                                    'sendcard_date' => $_POST['sendcard_date'],
-                                                    'zlfotmember_zlfotmember_id' => $_POST['zlfotmember_id'],
-                                                    'postoffice_postoffice_id' => $_POST['postoffice_postoffice_id']
-                                                    ));
-                                        }
-                            break;
-                           case "C":
-                            $data['zlfotcard_receiptfin'] = $_POST['zlfotcard_receiptfin'];
-                            break;
-                    }
+	if(!empty($_POST['zlfotcard_id'])){          
+                         $data['zlfotcard_status'] = $_POST['zlfotcard_status'];
                         $data['zlfotcard_stsfw'] = $_POST['zlfotcard_stsfw'];
                         $rsfix = $db->update('zlfotcard',$data,'zlfotcard_id',$_POST['zlfotcard_id']);
             
@@ -66,20 +39,7 @@
     	}else if($rsfix){
             echo "<div class='statusok'>แก้ไขสำเร็จ</div>";
                     }
-                    switch ($_POST['zlfotcard_stsfw']){
-                          case "P":
                              $link = "admin_index.php?url=zlfot_show_checkmember.php&type=2";
-                            break;
-                          case "S":
-                            $link = "admin_index.php?url=zlfot_show_checkmember.php&type=3";
-                            break;
-                          case "T":
-                            $link = "admin_index.php?url=zlfot_show_checkmember.php&type=4";
-                            break;
-                           case "C":
-                            $link = "admin_index.php?url=zlfot_show_member.php";
-                            break;
-                    }
             header( "Refresh: 2; $link" );
 }
 ?>
