79c409b4c6cad9bf9fb3250e8021047b279c7ba1
choatchaw <Choatchaw@live.com>
2020-08-03 14:56:03 +0700

    a

--- database/db_tools.php
+++ database/db_tools.php
@@ -421,7 +421,10 @@
 			$this->sql = "SELECT * FROM $table1,$table2,$table3,$table4,$table5,$table6,$table7,$table8 WHERE $column1 = $value1 && $column2 = $value2 && $column3 = $value3 && $column4 = $value4 && $column5 = $value5 && $column6 = $value6 && $column7 = $value7 && $column8 = $value8";
 			return $this;
 		}
-                
+                function findByPK89($table1,$table2,$table3,$table4,$table5,$table6,$table7,$table8,$column1,$value1,$column2,$value2,$column3,$value3,$column4,$value4,$column5,$value5,$column6,$value6,$column7,$value7,$column8,$value8,$column9,$value9){
+			$this->sql = "SELECT * FROM $table1,$table2,$table3,$table4,$table5,$table6,$table7,$table8 WHERE $column1 = $value1 && $column2 = $value2 && $column3 = $value3 && $column4 = $value4 && $column5 = $value5 && $column6 = $value6 && $column7 = $value7 && $column8 = $value8 && $column9 = $value9";
+			return $this;
+		}
         function findByPKOR24($table1,$table2,$column1,$value1,$column2,$value2,$column3,$value3,$column4,$value4){
     		$this->sql = "SELECT * FROM $table1,$table2 WHERE $column1 = $value1 && $column2 = $value2 && ($column3 = $value3 OR $column4 = $value4)";
 			return $this;
